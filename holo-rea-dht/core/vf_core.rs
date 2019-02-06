a03d84cc50b29dd303a76bec4856028b2c6b0a8c
pospi <pospi@spadgos.com>
2019-02-06 21:02:39 +1000

    include inline test for core VF module to see how such a feature pans out

--- holo-rea-dht/core/vf_core.rs
+++ holo-rea-dht/core/vf_core.rs
@@ -17,10 +17,21 @@ use hdk::holochain_core_types::{
  * VfEntry is the base class for entities that have to do with VF.
  * The standard says that there are a few fields that any object could have.
  */
-#[derive(Serialize, Deserialize, Debug, DefaultJson)]
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Default)]
 pub struct VfEntry {
   name: Option<String>,
   image: Option<String>,
   note: Option<String>,
   url: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn minimal_creation() {
+        let e: VfEntry = VfEntry { name: Some("Billy".into()), ..VfEntry::default() };
+        assert_eq!(e.name, Some("Billy".into()))
+    }
 }
