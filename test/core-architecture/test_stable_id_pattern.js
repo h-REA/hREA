19e64c9b187bb3aff8abf466ad2eaddd75d57fb0
pospi <pospi@spadgos.com>
2019-06-16 16:58:21 +1000

    test to validate that multiple updates with same ID function as expected

--- test/core-architecture/test_stable_id_pattern.js
+++ test/core-architecture/test_stable_id_pattern.js
@@ -29,4 +29,31 @@ runner.registerScenario('records have stable IDs after update', async (s, t, { o
   t.equal(updateEventResponse.economicEvent.note, 'updated event', 'update had no effect')
 })
 
+runner.registerScenario('records can be updated multiple times with same ID', async (s, t, { observation }) => {
+  const event = {
+    note: 'event v1',
+  }
+
+  const createResp = await observation.call('main', 'create_event', { event })
+
+  await observation.call('main', 'update_event', {
+    event: {
+      id: createResp.economicEvent.id,
+      note: 'event v2',
+    },
+  })
+  const updateResp2 = await observation.call('main', 'update_event', {
+    event: {
+      id: createResp.economicEvent.id,
+      note: 'event v3',
+    },
+  })
+
+  await s.consistent()
+
+  t.ok(updateResp2.economicEvent, 'subsequent update failed')
+  t.equal(updateResp2.economicEvent.note, 'event v3', 'subsequent update had no effect')
+  t.equal(createResp.economicEvent.id, updateResp2.economicEvent.id, 'ID changed after subsequent update')
+})
+
 runner.run()
