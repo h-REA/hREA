018f0a3f128c8b6cd726065edac8d315724573f2
pospi <pospi@spadgos.com>
2019-06-18 15:56:57 +1000

    tests for field update behaviours

---
+++ test/core-architecture/test_update_behavior.js
@@ -0,0 +1,57 @@
+const {
+  getDNA,
+  buildOrchestrator,
+} = require('../init')
+
+const runner = buildOrchestrator({
+  observation: getDNA('observation'),
+})
+
+runner.registerScenario('updates with fields ommitted leave original value intact', async (s, t, { observation }) => {
+  const event = {
+    note: 'test event',
+  }
+
+  const createEventResponse = await observation.call('main', 'create_event', { event })
+
+  t.ok(createEventResponse.Ok.economicEvent && createEventResponse.Ok.economicEvent.id, 'Creating record failed')
+
+  const updateEventResponse = await observation.call('main', 'update_event', {
+    event: {
+      id: createEventResponse.Ok.economicEvent.id,
+    },
+  })
+
+  await s.consistent()
+
+  const readResponse = await observation.call('main', 'get_event', { address: createEventResponse.Ok.economicEvent.id })
+
+  await s.consistent()
+
+  t.equal(readResponse.Ok.economicEvent.note, 'test event', 'field changed when not provided')
+})
+
+runner.registerScenario('updates with fields nulled remove original value', async (s, t, { observation }) => {
+  const event = {
+    note: 'test event 2',
+  }
+
+  const createEventResponse = await observation.call('main', 'create_event', { event })
+
+  t.ok(createEventResponse.Ok.economicEvent && createEventResponse.Ok.economicEvent.id, 'Creating record failed')
+
+  const updateEventResponse = await observation.call('main', 'update_event', {
+    event: {
+      id: createEventResponse.Ok.economicEvent.id,
+      note: null,
+    },
+  })
+
+  await s.consistent()
+
+  const readResponse = await observation.call('main', 'get_event', { address: createEventResponse.Ok.economicEvent.id })
+
+  await s.consistent()
+
+  t.equal(readResponse.Ok.economicEvent.note, null, 'field not removed')
+})
