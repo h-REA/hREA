c11cf2f8e867c1ec047537e51701df17c31185c2
pospi <pospi@spadgos.com>
2019-06-18 22:40:05 +1000

    remove defaults for measurement structs- not sane

--- lib/vf_core/src/measurement.rs
+++ lib/vf_core/src/measurement.rs
@@ -1,13 +1,13 @@
 use super::type_aliases::UnitAddress;
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct Unit {
     id: UnitAddress,
     name: Option<String>,
     symbol: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct QuantityValue {
     numeric_value: f32,  // :TODO: is this going to be wide enough in all cases?
     unit: UnitAddress,
