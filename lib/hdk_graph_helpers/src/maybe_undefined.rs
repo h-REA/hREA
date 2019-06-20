e9775a2aea62cd7d5dfc6f5ef8dfd519f341165d
pospi <pospi@spadgos.com>
2019-06-20 12:47:33 +1000

    complete tests for MaybeUndefined type

--- lib/hdk_graph_helpers/src/maybe_undefined.rs
+++ lib/hdk_graph_helpers/src/maybe_undefined.rs
@@ -65,9 +65,10 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::convert::TryFrom;
     use hdk::holochain_core_types_derive::{ DefaultJson };
 
-    #[derive(Serialize, Deserialize, Debug, DefaultJson, Default, Clone)]
+    #[derive(Serialize, Deserialize, Debug, DefaultJson, Default, Clone, PartialEq)]
     struct TestEntry {
         #[serde(default)]
         test_field: MaybeUndefined<Vec<String>>,
@@ -79,11 +80,52 @@ mod tests {
         }
     }
 
+    #[derive(Serialize, Deserialize, Debug, DefaultJson, Default, Clone, PartialEq)]
+    struct TestEntrySimple {
+        #[serde(default)]
+        test_field: MaybeUndefined<String>,
+    }
+
     // Ensures that pulling data out of optional fields is possible when the data has move semantics
     #[test]
     fn test_vector_ownership() {
         let entry = TestEntry { test_field: MaybeUndefined::Some(vec!["blah".to_string()]) };
         let copied = entry.getter();
         let another: TestEntry = entry.into();
+    }
+
+    #[test]
+    fn test_deserialization_some() {
+        let expected = TestEntrySimple { test_field: MaybeUndefined::Some("blah".to_string()) };
+        let input_json = "{\"test_field\":\"blah\"}";
+
+        assert_eq!(
+            Ok(expected),
+            TestEntrySimple::try_from(JsonString::from_json(input_json)),
+        );
+    }
+
+    #[test]
+    fn test_deserialization_none() {
+        let expected = TestEntrySimple { test_field: MaybeUndefined::None };
+        let input_json = "{\"test_field\":null}";
+
+        assert_eq!(
+            Ok(expected),
+            TestEntrySimple::try_from(JsonString::from_json(input_json)),
+        );
+    }
+
+    #[test]
+    fn test_deserialization_undefined() {
+        assert_eq!(
+            Ok(TestEntrySimple { test_field: MaybeUndefined::Undefined }),
+            TestEntrySimple::try_from(JsonString::from_json("{}")),
+        );
+
+        assert_ne!(
+            Ok(TestEntrySimple { test_field: MaybeUndefined::Undefined }),
+            TestEntrySimple::try_from(JsonString::from_json("{\"test_field\":null}")),
+        );
     }
 }
