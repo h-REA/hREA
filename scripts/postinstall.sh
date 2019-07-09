48d73216c364dd4c044962ebab08c20aae2eccc3
pospi <pospi@spadgos.com>
2019-07-09 18:56:56 +1000

    update postinstall script for Nix installation if Nix is found

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -13,35 +13,54 @@ HDK_RUST_REVID=v0.0.19-alpha1
 
 DEP_ERR_OUTTRO="Please see README for setup instructions."
 
-command -v rustup >/dev/null 2>&1 || {
-  echo -e "\e[1m\e[31mRust not installed! $DEP_ERR_OUTTRO">&2;
-  exit 1
-}
+HAS_NIX=$(command -v nix-shell >/dev/null 2>&1)
+HAS_NIX=$?
 
-# activate & lock rust toolchain for this repo
-rustup override set nightly-2019-02-04
+if [[ $HAS_NIX ]]; then
+  echo "Nix installed... booting initial shell"
+  echo "Use this command in future:"
+  echo "    nix-shell https://holochain.love"
+  echo ""
+  echo "Hit CTRL-D to exit when ready."
 
-# install rust deps
-rustup target add wasm32-unknown-unknown
-rustup component add clippy
-cargo install rustfmt
-# install Holochain CLI and runtime at known compatible version
-cargo install holochain hc --git https://github.com/holochain/holochain-rust.git --tag $HDK_RUST_REVID
+  nix-shell https://holochain.love
+else
+  echo -e "Nix not installed... seeing if you have Rust natively...">&2;
+
+  command -v rustup >/dev/null 2>&1 || {
+    echo -e "\e[1m\e[31mRust not installed! $DEP_ERR_OUTTRO">&2;
+    exit 1
+  }
+
+  # RUST-BASED INSTALL
+
+  # activate & lock rust toolchain for this repo
+  rustup override set nightly-2019-02-04
+
+  # install rust deps
+  rustup target add wasm32-unknown-unknown
+  rustup component add clippy
+  cargo install rustfmt
+  # install Holochain CLI and runtime at known compatible version
+  cargo install holochain hc --git https://github.com/holochain/holochain-rust.git --tag $HDK_RUST_REVID
+
+  # END INSTALL
+
+  # post-install check
+  command -v rustfmt >/dev/null 2>&1 || {
+    echo -e "\e[1m\e[31mRustfmt not installed! $DEP_ERR_OUTTRO">&2;
+    exit 1
+  }
+  command -v hc >/dev/null 2>&1 || {
+    echo -e "\e[1m\e[31mHolochain CLI not installed! $DEP_ERR_OUTTRO">&2;
+    exit 1
+  }
+  command -v holochain >/dev/null 2>&1 || {
+    echo -e "\e[1m\e[31mHolochain runtime not installed! $DEP_ERR_OUTTRO">&2;
+    exit 1
+  }
+fi
 
 # munge some stuff for development ease-of-use
 # (note CWD is root folder as this script is driven by NPM)
 node scripts/fixReactAppSourceMaps.js
-
-# post-install check
-command -v rustfmt >/dev/null 2>&1 || {
-  echo -e "\e[1m\e[31mRustfmt not installed! $DEP_ERR_OUTTRO">&2;
-  exit 1
-}
-command -v hc >/dev/null 2>&1 || {
-  echo -e "\e[1m\e[31mHolochain CLI not installed! $DEP_ERR_OUTTRO">&2;
-  exit 1
-}
-command -v holochain >/dev/null 2>&1 || {
-  echo -e "\e[1m\e[31mHolochain runtime not installed! $DEP_ERR_OUTTRO">&2;
-  exit 1
-}
