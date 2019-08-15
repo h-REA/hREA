c96f04775938135e2d9157876968bfe187379ac4
pospi <pospi@spadgos.com>
2019-08-15 20:28:47 +1000

    implement handler for event update in GraphQL connector

--- modules/vf-graphql-holochain/mutations/economicEvent.ts
+++ modules/vf-graphql-holochain/mutations/economicEvent.ts
@@ -9,11 +9,13 @@ import { zomeFunction } from '../connection'
 import {
   EconomicEventCreateParams,
   EconomicResourceCreateParams,
+  EconomicEventUpdateParams,
   EconomicEventResponse,
 } from '@valueflows/vf-graphql'
 
 // :TODO: how to inject DNA identifier?
 const createEvent = zomeFunction('a1_observation', 'economic_event', 'create_event')
+const updateEvent = zomeFunction('a1_observation', 'economic_event', 'update_event')
 
 // CREATE
 interface CreateArgs {
@@ -25,4 +27,15 @@ type createHandler = (root: any, args: CreateArgs) => Promise<EconomicEventRespo
 export const createEconomicEvent: createHandler = async (root, args) => {
   const { event, createResource } = args
   return (await createEvent)({ event, createResource })
+}
+
+// UPDATE
+interface UpdateArgs {
+  event: EconomicEventUpdateParams,
+}
+type updateHandler = (root: any, args: UpdateArgs) => Promise<EconomicEventResponse>
+
+export const updateEconomicEvent: updateHandler = async (root, args) => {
+  const { event } = args
+  return (await updateEvent)({ event })
 }
