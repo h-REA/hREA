b026c6353acb89ce7301488f2a2e5a198537f132
pospi <pospi@spadgos.com>
2019-08-29 13:35:28 +1000

    finish fulfillment link traversal logic

--- modules/vf-graphql-holochain/resolvers/fulfillment.ts
+++ modules/vf-graphql-holochain/resolvers/fulfillment.ts
@@ -10,18 +10,18 @@ import { zomeFunction } from '../connection'
 
 import {
   Fulfillment,
+  EconomicEvent,
+  Commitment,
 } from '@valueflows/vf-graphql'
 
 // :TODO: how to inject DNA identifier?
 const readEvents = zomeFunction('a1_observation', 'economic_event', 'query_events')
 const readCommitments = zomeFunction('a1_planning', 'commitment', 'query_commitments')
 
-export const fulfilledBy = async ({ id }: Fulfillment) => {
-  const resp = await (await readEvents)({ fulfills: id })
-  return resp
+export const fulfilledBy = async (record: Fulfillment): Promise<EconomicEvent> => {
+  return (await (await readEvents)({ fulfills: record.id })).map(({ economicEvent }) => economicEvent)[0]
 }
 
-export const fulfills = async ({ id }: Fulfillment) => {
-  const resp = await (await readCommitments)({ fulfilledBy: id })
-  return resp
+export const fulfills = async (record: Fulfillment): Promise<Commitment> => {
+  return (await (await readCommitments)({ fulfilled_by: record.id })).map(({ commitment }) => commitment)[0]
 }
