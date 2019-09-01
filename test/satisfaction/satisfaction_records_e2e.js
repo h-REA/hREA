0edecbc71aab5bf27d1c6a71b019a09d83149d75
pospi <pospi@spadgos.com>
2019-09-01 10:12:39 +1000

    update query API in tests

--- test/satisfaction/satisfaction_records_e2e.js
+++ test/satisfaction/satisfaction_records_e2e.js
@@ -61,19 +61,24 @@ runner.registerScenario('satisfactions can be written and read between DNAs by a
   t.equal(readResponse.Ok.intent.satisfiedBy[0], satisfactionId, 'Intent.satisfiedBy reciprocal satisfaction reference OK')
 
   // ASSERT: check intent query indexes
-  readResponse = await planning.call('satisfaction', 'query_satisfactions', { satisfies: intentId })
+  readResponse = await planning.call('satisfaction', 'query_satisfactions', { params: { satisfies: intentId } })
   t.equal(readResponse.Ok.length, 1, 'read satisfactions by intent OK')
   t.equal(readResponse.Ok[0].satisfaction.id, satisfactionId, 'Satisfaction.satisfies indexed correctly')
 
   // ASSERT: check event query indexes
-  readResponse = await observation.call('satisfaction', 'query_satisfactions', { satisfied_by: eventId })
+  readResponse = await observation.call('satisfaction', 'query_satisfactions', { params: { satisfiedBy: eventId } })
   t.equal(readResponse.Ok.length, 1, 'read satisfactions by event OK')
   t.equal(readResponse.Ok[0].satisfaction.id, satisfactionId, 'Satisfaction.satisfiedBy indexed correctly')
 
   // ASSERT: check intent satisfaction query indexes
-  readResponse = await planning.call('intent', 'query_intents', { satisfied_by: satisfactionId })
+  readResponse = await planning.call('intent', 'query_intents', { params: { satisfiedBy: satisfactionId } })
   t.equal(readResponse.Ok.length, 1, 'indexing satisfactions for intent query OK')
   t.equal(readResponse.Ok[0].intent.id, intentId, 'intent query 1 indexed correctly')
+
+  // ASSERT: check event satisfaction query indexes
+  readResponse = await observation.call('event', 'query_events', { params: { satisfies: satisfactionId } })
+  t.equal(readResponse.Ok.length, 1, 'indexing satisfactions for event query OK')
+  t.equal(readResponse.Ok[0].economicEvent.id, intentId, 'event query 1 indexed correctly')
 
 
 
@@ -97,7 +102,7 @@ runner.registerScenario('satisfactions can be written and read between DNAs by a
   const satisfactionId2 = satisfactionResp2.Ok.satisfaction.id
 
   // ASSERT: check intent query indices
-  readResponse = await planning.call('satisfaction', 'query_satisfactions', { satisfies: intentId })
+  readResponse = await planning.call('satisfaction', 'query_satisfactions', { params: { satisfies: intentId } })
   t.equal(readResponse.Ok.length, 2, 'appending satisfactions for read OK')
   t.equal(readResponse.Ok[0].satisfaction.id, satisfactionId, 'satisfaction 1 indexed correctly')
   t.equal(readResponse.Ok[1].satisfaction.id, satisfactionId2, 'satisfaction 2 indexed correctly')
@@ -109,12 +114,12 @@ runner.registerScenario('satisfactions can be written and read between DNAs by a
   t.equal(readResponse.Ok.intent.satisfiedBy[1], satisfactionId2, 'Intent.satisfiedBy reference 2 OK')
 
   // ASSERT: check commitment query indexes
-  readResponse = await observation.call('satisfaction', 'query_satisfactions', { satisfied_by: commitmentId })
+  readResponse = await planning.call('satisfaction', 'query_satisfactions', { params: { satisfiedBy: commitmentId } })
   t.equal(readResponse.Ok.length, 1, 'read satisfactions by commitment OK')
   t.equal(readResponse.Ok[0].satisfaction.id, satisfactionId, 'Satisfaction.satisfiedBy indexed correctly')
 
   // ASSERT: check intent satisfaction query indexes
-  readResponse = await planning.call('intent', 'query_intents', { satisfied_by: satisfactionId2 })
+  readResponse = await planning.call('intent', 'query_intents', { params: { satisfiedBy: satisfactionId2 } })
   t.equal(readResponse.Ok.length, 1, 'appending satisfactions for intent query OK')
   t.equal(readResponse.Ok[0].intent.id, intentId, 'intent query 2 indexed correctly')
 })
