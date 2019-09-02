c929ff4f9ec4168cfdd0532f435491084ac8c118
pospi <pospi@spadgos.com>
2019-09-02 16:01:29 +1000

    add consistency waits to update behaviour test (though still hangs conductor)

--- test/core-architecture/test_update_behavior.js
+++ test/core-architecture/test_update_behavior.js
@@ -13,19 +13,17 @@ runner.registerScenario('updates with fields ommitted leave original value intac
   }
 
   const createEventResponse = await observation.call('economic_event', 'create_event', { event })
-
   t.ok(createEventResponse.Ok.economicEvent && createEventResponse.Ok.economicEvent.id, 'record created successfully')
+  await s.consistent()
 
   const updateEventResponse = await observation.call('economic_event', 'update_event', {
     event: {
       id: createEventResponse.Ok.economicEvent.id,
     },
   })
-
   await s.consistent()
 
   const readResponse = await observation.call('economic_event', 'get_event', { address: createEventResponse.Ok.economicEvent.id })
-
   t.equal(readResponse.Ok.economicEvent.note, 'test event', 'field remains if not provided')
 })
 
@@ -35,8 +33,8 @@ runner.registerScenario('updates with fields nulled remove original value', asyn
   }
 
   const createEventResponse = await observation.call('economic_event', 'create_event', { event })
-
   t.ok(createEventResponse.Ok.economicEvent && createEventResponse.Ok.economicEvent.id, 'record created successfully')
+  await s.consistent()
 
   const updateEventResponse = await observation.call('economic_event', 'update_event', {
     event: {
@@ -44,11 +42,9 @@ runner.registerScenario('updates with fields nulled remove original value', asyn
       note: null,
     },
   })
-
   await s.consistent()
 
   const readResponse = await observation.call('economic_event', 'get_event', { address: createEventResponse.Ok.economicEvent.id })
-
   t.equal(readResponse.Ok.economicEvent.note, undefined, 'field removed if nulled')
 })
 
