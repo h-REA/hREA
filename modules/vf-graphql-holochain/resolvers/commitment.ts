97a5be944f4b33c0ff6c78abf8dbcdcd5b2d3fff
pospi <pospi@spadgos.com>
2019-09-04 12:13:10 +1000

    implement missed GraphQL resolver for Commitment.satisfies

--- modules/vf-graphql-holochain/resolvers/commitment.ts
+++ modules/vf-graphql-holochain/resolvers/commitment.ts
@@ -10,11 +10,17 @@ import { zomeFunction } from '../connection'
 import {
   Commitment,
   Fulfillment,
+  Satisfaction,
 } from '@valueflows/vf-graphql'
 
 // :TODO: how to inject DNA identifier?
 const readFulfillments = zomeFunction('a1_planning', 'fulfillment', 'query_fulfillments')
+const readSatisfactions = zomeFunction('a1_planning', 'satisfaction', 'query_satisfactions')
 
 export const fulfilledBy = async (record: Commitment): Promise<[Fulfillment]> => {
   return (await (await readFulfillments)({ commitment: record.id })).map(({ fulfillment }) => fulfillment)
+}
+
+export const satisfies = async (record: Commitment): Promise<[Satisfaction]> => {
+  return (await (await readSatisfactions)({ satisfied_by: record.id })).map(({ satisfaction }) => satisfaction)
 }
