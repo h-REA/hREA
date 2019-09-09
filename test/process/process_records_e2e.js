90c8e05ce734dc10d8a11d54840a7ce84eef45f4
pospi <pospi@spadgos.com>
2019-09-09 14:53:28 +1000

    intent & commitment process link tests

--- test/process/process_records_e2e.js
+++ test/process/process_records_e2e.js
@@ -36,10 +36,49 @@ runner.registerScenario('process query indexes and relationships', async (s, t, 
   }
   const oeResp = await observation.call('economic_event', 'create_event', { event: oEvent })
   t.ok(oeResp.Ok.economicEvent && oeResp.Ok.economicEvent.id, 'output event created successfully')
+  t.equal(oeResp.Ok.economicEvent.outputOf, processId, 'event.outputOf reference OK in write')
   await s.consistent()
   const oEventId = oeResp.Ok.economicEvent.id
 
-  // :TODO: commitment, intent
+  const iCommitment = {
+    note: 'test input commitment',
+    inputOf: processId,
+  }
+  const icResp = await planning.call('commitment', 'create_commitment', { commitment: iCommitment })
+  t.ok(icResp.Ok.commitment && icResp.Ok.commitment.id, 'input commitment created successfully')
+  t.equal(icResp.Ok.commitment.inputOf, processId, 'commitment.inputOf reference OK in write')
+  await s.consistent()
+  const iCommitmentId = icResp.Ok.commitment.id
+
+  const oCommitment = {
+    note: 'test output commitment',
+    outputOf: processId,
+  }
+  const ocResp = await planning.call('commitment', 'create_commitment', { commitment: oCommitment })
+  t.ok(ocResp.Ok.commitment && ocResp.Ok.commitment.id, 'output commitment created successfully')
+  t.equal(ocResp.Ok.commitment.outputOf, processId, 'commitment.outputOf reference OK in write')
+  await s.consistent()
+  const oCommitmentId = ocResp.Ok.commitment.id
+
+  const iIntent = {
+    note: 'test input intent',
+    inputOf: processId,
+  }
+  const iiResp = await planning.call('intent', 'create_intent', { intent: iIntent })
+  t.ok(iiResp.Ok.intent && iiResp.Ok.intent.id, 'input intent created successfully')
+  t.equal(iiResp.Ok.intent.inputOf, processId, 'intent.inputOf reference OK in write')
+  await s.consistent()
+  const iIntentId = iiResp.Ok.intent.id
+
+  const oIntent = {
+    note: 'test output intent',
+    outputOf: processId,
+  }
+  const oiResp = await planning.call('intent', 'create_intent', { intent: oIntent })
+  t.ok(oiResp.Ok.intent && oiResp.Ok.intent.id, 'output intent created successfully')
+  t.equal(oiResp.Ok.intent.outputOf, processId, 'intent.outputOf reference OK in write')
+  await s.consistent()
+  const oIntentId = oiResp.Ok.intent.id
 
 
 
@@ -48,6 +87,7 @@ runner.registerScenario('process query indexes and relationships', async (s, t, 
   t.ok(readResponse.Ok.economicEvent && readResponse.Ok.economicEvent.inputOf, 'EconomicEvent.inputOf index saved')
   t.equal(readResponse.Ok.economicEvent && readResponse.Ok.economicEvent.inputOf, processId, 'EconomicEvent.inputOf reference OK in read')
 
+  // ASSERT: check output event index links
   readResponse = await observation.call('economic_event', 'get_event', { address: oEventId })
   t.ok(readResponse.Ok.economicEvent && readResponse.Ok.economicEvent.outputOf, 'EconomicEvent.outputOf index saved')
   t.equal(readResponse.Ok.economicEvent && readResponse.Ok.economicEvent.outputOf, processId, 'EconomicEvent.outputOf reference OK in read')
@@ -73,7 +113,71 @@ runner.registerScenario('process query indexes and relationships', async (s, t, 
   t.equal(readResponse.Ok[0] && readResponse.Ok[0].process && readResponse.Ok[0].process.id, processId, 'process.outputs query index created')
 
 
-  // SCENARIO: modify events
+
+  // ASSERT: check input commitment index links
+  readResponse = await observation.call('commitment', 'get_commitment', { address: iCommitmentId })
+  t.ok(readResponse.Ok.commitment && readResponse.Ok.commitment.inputOf, 'commitment.inputOf index saved')
+  t.equal(readResponse.Ok.commitment && readResponse.Ok.commitment.inputOf, processId, 'commitment.inputOf reference OK in read')
+
+  // ASSERT: check output commitment index links
+  readResponse = await observation.call('commitment', 'get_commitment', { address: oCommitmentId })
+  t.ok(readResponse.Ok.commitment && readResponse.Ok.commitment.outputOf, 'commitment.outputOf index saved')
+  t.equal(readResponse.Ok.commitment && readResponse.Ok.commitment.outputOf, processId, 'commitment.outputOf reference OK in read')
+
+  // ASSERT: test commitment input query edge
+  readResponse = await observation.call('commitment', 'query_commitments', { params: { inputOf: processId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'commitment input query index present')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].commitment && readResponse.Ok[0].commitment.id, iCommitmentId, 'commitment input query index created')
+
+  // ASSERT: test commitment output query edge
+  readResponse = await observation.call('commitment', 'query_commitments', { params: { outputOf: processId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'commitment output query index present')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].commitment && readResponse.Ok[0].commitment.id, oCommitmentId, 'commitment output query index created')
+
+  // ASSERT: check process commitment input query edge
+  readResponse = await observation.call('process', 'query_processes', { params: { committedInputs: iCommitmentId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'process.inputs query succeeded')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].process && readResponse.Ok[0].process.id, processId, 'process.committedInputs query index created')
+
+  // ASSERT: check process commitment output query edge
+  readResponse = await observation.call('process', 'query_processes', { params: { committedOutputs: oCommitmentId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'process.outputs query succeeded')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].process && readResponse.Ok[0].process.id, processId, 'process.committedOutputs query index created')
+
+
+
+  // ASSERT: check input intent index links
+  readResponse = await observation.call('intent', 'get_intent', { address: iIntentId })
+  t.ok(readResponse.Ok.intent && readResponse.Ok.intent.inputOf, 'intent.inputOf index saved')
+  t.equal(readResponse.Ok.intent && readResponse.Ok.intent.inputOf, processId, 'intent.inputOf reference OK in read')
+
+  // ASSERT: check output intent index links
+  readResponse = await observation.call('intent', 'get_intent', { address: oIntentId })
+  t.ok(readResponse.Ok.intent && readResponse.Ok.intent.outputOf, 'intent.outputOf index saved')
+  t.equal(readResponse.Ok.intent && readResponse.Ok.intent.outputOf, processId, 'intent.outputOf reference OK in read')
+
+  // ASSERT: test intent input query edge
+  readResponse = await observation.call('intent', 'query_intents', { params: { inputOf: processId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'intent input query index present')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].intent && readResponse.Ok[0].intent.id, iIntentId, 'intent input query index created')
+
+  // ASSERT: test intent output query edge
+  readResponse = await observation.call('intent', 'query_intents', { params: { outputOf: processId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'intent output query index present')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].intent && readResponse.Ok[0].intent.id, oIntentId, 'intent output query index created')
+
+  // ASSERT: check process intent input query edge
+  readResponse = await observation.call('process', 'query_processes', { params: { committedInputs: iIntentId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'process.inputs query succeeded')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].process && readResponse.Ok[0].process.id, processId, 'process.committedInputs query index created')
+
+  // ASSERT: check process intent output query edge
+  readResponse = await observation.call('process', 'query_processes', { params: { committedOutputs: oIntentId } })
+  t.equal(readResponse.Ok && readResponse.Ok.length, 1, 'process.outputs query succeeded')
+  t.equal(readResponse.Ok[0] && readResponse.Ok[0].process && readResponse.Ok[0].process.id, processId, 'process.committedOutputs query index created')
+
+
+  // TODO: modify
 })
 
 runner.run()
