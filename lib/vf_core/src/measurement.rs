00037c59f259a1ae950ac168876f73a8bd80e5bb
pospi <pospi@spadgos.com>
2019-09-16 21:52:55 +1000

    QuantityValue add / subtract implementation

--- lib/vf_core/src/measurement.rs
+++ lib/vf_core/src/measurement.rs
@@ -12,3 +12,23 @@ pub struct QuantityValue {
     numeric_value: f32,  // :TODO: is this going to be wide enough in all cases?
     unit: UnitAddress,
 }
+
+pub fn add(q1: QuantityValue, q2: QuantityValue) -> QuantityValue {
+    if q1.unit != q2.unit {
+        panic!("Unimplemented! Need to enable unit conversions in QuantityValue math");
+    }
+    QuantityValue {
+        numeric_value: q1.numeric_value + q2.numeric_value,
+        unit: q1.unit,
+    }
+}
+
+pub fn subtract(q1: QuantityValue, q2: QuantityValue) -> QuantityValue {
+    if q1.unit != q2.unit {
+        panic!("Unimplemented! Need to enable unit conversions in QuantityValue math");
+    }
+    QuantityValue {
+        numeric_value: q1.numeric_value - q2.numeric_value,
+        unit: q1.unit,
+    }
+}
