19c7d42e9383847da7f9bb2acc90f4ce4b75ad84
pospi <pospi@spadgos.com>
2019-09-17 16:31:06 +1000

    wire up resource update logic that can be inferred directly from existing event schema

--- lib/vf_observation/src/economic_resource.rs
+++ lib/vf_observation/src/economic_resource.rs
@@ -54,8 +54,6 @@ pub struct Entry {
 pub struct CreateRequest {
     conforms_to: ResourceSpecificationAddress,
     #[serde(default)]
-    classified_as: MaybeUndefined<Vec<ExternalURL>>,
-    #[serde(default)]
     tracking_identifier: MaybeUndefined<String>,
     #[serde(default)]
     lot: MaybeUndefined<ProductBatchAddress>,
@@ -65,6 +63,8 @@ pub struct CreateRequest {
     accounting_quantity: MaybeUndefined<QuantityValue>,
     #[serde(default)]
     onhand_quantity: MaybeUndefined<QuantityValue>,
+    #[serde(default)]
+    unit_of_effort: MaybeUndefined<UnitAddress>,
     #[serde(default)]
     contained_in: MaybeUndefined<ResourceAddress>,
     #[serde(default)]
@@ -97,8 +97,6 @@ pub struct UpdateRequest {
     #[serde(default)]
     contained_in: MaybeUndefined<ResourceAddress>,
     #[serde(default)]
-    stage: MaybeUndefined<ProcessSpecificationAddress>,
-    #[serde(default)]
     current_location: MaybeUndefined<LocationAddress>,
     #[serde(default)]
     note: MaybeUndefined<String>,
@@ -110,22 +108,43 @@ impl<'a> UpdateRequest {
     }
 }
 
+pub struct CreationPayload {
+    event: EventCreateRequest,
+    resource: CreateRequest,
+}
+
+pub fn resource_creation(event: &EventCreateRequest, resource: &CreateRequest) -> CreationPayload {
+    CreationPayload {
+        event: event.to_owned(),
+        resource: resource.to_owned(),
+    }
+}
+
 /// Handles create operations via observed event resource inspection parameter
-impl From<CreateRequest> for Entry {
-    fn from(e: CreateRequest) -> Entry {
+/// @see https://github.com/holo-rea/holo-rea/issues/65
+impl From<CreationPayload> for Entry
+{
+    fn from(t: CreationPayload) -> Entry {
+        let r = t.resource;
+        let e = t.event;
         Entry {
-            conforms_to: e.conforms_to.to_owned(),
-            classified_as: if e.classified_as == MaybeUndefined::Undefined { None } else { e.classified_as.to_owned().to_option() },
-            tracking_identifier: if e.tracking_identifier == MaybeUndefined::Undefined { None } else { e.tracking_identifier.to_owned().to_option() },
-            lot: if e.lot == MaybeUndefined::Undefined { None } else { e.lot.to_owned().to_option() },
-            image: if e.image == MaybeUndefined::Undefined { None } else { e.image.to_owned().to_option() },
-            accounting_quantity: if e.accounting_quantity == MaybeUndefined::Undefined { None } else { e.accounting_quantity.to_owned().to_option() },
-            onhand_quantity: if e.onhand_quantity == MaybeUndefined::Undefined { None } else { e.onhand_quantity.to_owned().to_option() },
-            unit_of_effort: None,   // :TODO:
-            stage: None, // :TODO: check this
-            state: None, // :TODO: check this if e.state == MaybeUndefined::Undefined { None } else { e.state.clone().into() },
-            current_location: if e.current_location == MaybeUndefined::Undefined { None } else { e.current_location.to_owned().to_option() },
-            note: if e.note == MaybeUndefined::Undefined { None } else { e.note.clone().into() },
+            conforms_to: r.conforms_to.to_owned(),
+            classified_as: if e.resource_classified_as == MaybeUndefined::Undefined { None } else { e.resource_classified_as.to_owned().to_option() },
+            tracking_identifier: if r.tracking_identifier == MaybeUndefined::Undefined { None } else { r.tracking_identifier.to_owned().to_option() },
+            lot: if r.lot == MaybeUndefined::Undefined { None } else { r.lot.to_owned().to_option() },
+            image: if r.image == MaybeUndefined::Undefined { None } else { r.image.to_owned().to_option() },
+            accounting_quantity: if r.accounting_quantity == MaybeUndefined::Undefined { None } else { r.accounting_quantity.to_owned().to_option() },
+            onhand_quantity: if r.onhand_quantity == MaybeUndefined::Undefined { None } else { r.onhand_quantity.to_owned().to_option() },
+            unit_of_effort: if r.unit_of_effort == MaybeUndefined::Undefined {
+                None // :TODO: pull from e.resource_conforms_to.unit_of_effort if present
+            } else { r.unit_of_effort.to_owned().to_option() },
+            stage: None, // :TODO: pull from e.output_of.based_on if present. Undecided whether this should only happen on 'modify' events, or on everything.
+            state: match String::from(e.action.clone()).as_ref() {
+                "pass" | "fail" => Some(e.action),
+                _ => None,
+            },
+            current_location: if r.current_location == MaybeUndefined::Undefined { None } else { r.current_location.to_owned().to_option() },
+            note: if r.note == MaybeUndefined::Undefined { None } else { r.note.clone().into() },
         }
     }
 }
@@ -142,8 +161,8 @@ impl Updateable<UpdateRequest> for Entry {
             accounting_quantity: if e.accounting_quantity == MaybeUndefined::Undefined { self.accounting_quantity.to_owned() } else { e.accounting_quantity.to_owned().to_option() },
             onhand_quantity: if e.onhand_quantity == MaybeUndefined::Undefined { self.onhand_quantity.to_owned() } else { e.onhand_quantity.to_owned().to_option() },
             unit_of_effort: if e.unit_of_effort == MaybeUndefined::Undefined { self.unit_of_effort.to_owned() } else { e.unit_of_effort.to_owned().to_option() },
-            stage: if e.stage == MaybeUndefined::Undefined { self.stage.to_owned() } else { e.stage.to_owned().to_option() },
-            state: self.state.to_owned(), // :TODO: check this if e.state == MaybeUndefined::Undefined { self.state } else { e.state.to_option() },
+            stage: self.stage.to_owned(),
+            state: self.state.to_owned(),
             current_location: if e.current_location == MaybeUndefined::Undefined { self.current_location.to_owned() } else { e.current_location.to_owned().to_option() },
             note: if e.note == MaybeUndefined::Undefined { self.note.to_owned() } else { e.note.to_owned().to_option() },
         }
@@ -154,33 +173,46 @@ impl Updateable<UpdateRequest> for Entry {
 impl Updateable<EventCreateRequest> for Entry {
     fn update_with(&self, e: &EventCreateRequest) -> Entry {
         Entry {
-            conforms_to: self.conforms_to.to_owned(),
-            classified_as: self.classified_as.to_owned(),
+            conforms_to: if let MaybeUndefined::Some(conforms_to) = e.resource_conforms_to.to_owned() { conforms_to } else { self.conforms_to.to_owned() },
+            classified_as: if let MaybeUndefined::Some(classified_as) = e.resource_classified_as.to_owned() {
+                let mut results: Vec<ExternalURL> = if let Some(already_classified) = &self.classified_as { already_classified.to_owned() } else { vec![] };
+                results.append(&mut classified_as.clone());
+                Some(results)
+            } else { self.classified_as.to_owned() },
             tracking_identifier: self.tracking_identifier.to_owned(),
             lot: self.lot.to_owned(),
             image: self.image.to_owned(),
-            accounting_quantity: if let Some(qty) = self.accounting_quantity.to_owned() {
-                if let MaybeUndefined::Some(event_resource_qty) = e.resource_quantity.to_owned() {
-                    match get_builtin_action(e.action.as_ref()) {
-                        Some(action) => {
-                            match action.resource_effect {
-                                ActionEffect::Increment => Some(add(qty, event_resource_qty)),
-                                ActionEffect::Decrement => Some(subtract(qty, event_resource_qty)),
-                                _ => self.accounting_quantity.to_owned(),
-                            }
-                        },
-                        None => self.accounting_quantity.to_owned(),
-                    }
-                } else { self.accounting_quantity.to_owned() }
-            } else { None },
-            onhand_quantity: self.onhand_quantity.to_owned(),  // :TODO: how is this affected?
-            unit_of_effort: self.unit_of_effort.to_owned(),
-            stage: self.stage.to_owned(),
+            accounting_quantity: update_quantity(self.accounting_quantity.to_owned(), e.resource_quantity.to_owned(), e.action.as_ref()),
+            onhand_quantity: update_quantity(self.onhand_quantity.to_owned(), e.resource_quantity.to_owned(), e.action.as_ref()),
+            unit_of_effort: self.unit_of_effort.to_owned(), // :TODO: pull from e.resource_conforms_to.unit_of_effort
+            stage: self.stage.to_owned(), // :TODO: pull from e.output_of.based_on if present
             state: self.state.to_owned(),
             current_location: self.current_location.to_owned(),
             note: self.note.to_owned(),
         }
     }
+}
+
+/// Encapsulates the logic for updating EconomicResource quantities in response to event triggers
+fn update_quantity(
+    current_val: Option<QuantityValue>,
+    event_qty: MaybeUndefined<QuantityValue>,
+    action: &str,
+) -> Option<QuantityValue> {
+    if let Some(qty) = current_val.to_owned() {
+        if let MaybeUndefined::Some(event_resource_qty) = event_qty.to_owned() {
+            match get_builtin_action(action) {
+                Some(action) => {
+                    match action.resource_effect {
+                        ActionEffect::Increment => Some(add(qty, event_resource_qty)),
+                        ActionEffect::Decrement => Some(subtract(qty, event_resource_qty)),
+                        _ => current_val.to_owned(),
+                    }
+                },
+                None => current_val.to_owned(),
+            }
+        } else { current_val.to_owned() }
+    } else { None }
 }
 
 /// Handle updates when a previously logged event is altered after application
