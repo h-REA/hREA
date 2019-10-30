daffc1cde1f33ee6e4041cea1caf8a9b9a6cfce9
pospi <pospi@spadgos.com>
2019-10-30 13:33:52 +1000

    update flow graphQL test to add a Process & links

--- test/flows/flow_records_graphql.js
+++ test/flows/flow_records_graphql.js
@@ -15,48 +15,115 @@ const config = buildConfig({
 runner.registerScenario('flow records and relationships', async (s, t) => {
   const { alice } = await s.players({ alice: config }, true)
 
+  const tempProviderAgentId = 'some-agent-provider'
+  const tempReceiverAgentId = 'some-agent-receiver'
+
+  const pResp = await graphQL(`
+    mutation($process: ProcessCreateParams!) {
+      createProcess(process: $process) {
+        process {
+          id
+        }
+      }
+    }
+  `, {
+    process: {
+      name: 'test process for linking logic',
+    }
+  })
+  await s.consistency()
+
+  t.ok(pResp.data.createProcess.process.id, "process created OK")
+  const processId = pResp.data.createProcess.process.id
+
   const cResp = await graphQL(`
     mutation(
-      $event: EconomicEventCreateParams!,
-      $commitment: CommitmentCreateParams!,
-      $intent: IntentCreateParams!
+      $eventI: EconomicEventCreateParams!,
+      $commitmentI: CommitmentCreateParams!,
+      $intentI: IntentCreateParams!,
+      $eventO: EconomicEventCreateParams!,
+      $commitmentO: CommitmentCreateParams!,
+      $intentO: IntentCreateParams!
     ) {
-      createIntent(intent: $intent) {
+      inputIntent: createIntent(intent: $intentI) {
         intent {
           id
         }
       }
-      createCommitment(commitment: $commitment) {
+      inputCommitment: createCommitment(commitment: $commitmentI) {
         commitment {
           id
         }
       }
-      createEconomicEvent(event:$event) {
+      inputEvent: createEconomicEvent(event: $eventI) {
+        economicEvent {
+          id
+        }
+      }
+      outputIntent: createIntent(intent: $intentO) {
+        intent {
+          id
+        }
+      }
+      outputCommitment: createCommitment(commitment: $commitmentO) {
+        commitment {
+          id
+        }
+      }
+      outputEvent: createEconomicEvent(event: $eventO) {
         economicEvent {
           id
         }
       }
     }
   `, {
-    "intent": {
+    "intentI": {
+      "action": "consume",
+      "inputOf": processId,
+      "note": "some input is required"
+    },
+    "commitmentI": {
+      "action": "consume",
+      "inputOf": processId,
+      "provider": tempProviderAgentId,
+      "receiver": tempReceiverAgentId,
+      "note": "some input will be provided"
+    },
+    "eventI": {
+      "action": "consume",
+      "inputOf": processId,
+      "provider": tempProviderAgentId,
+      "receiver": tempReceiverAgentId,
+      "note": "some input was used up"
+    },
+    "intentO": {
       "action": "produce",
+      "outputOf": processId,
       "note": "please make the thing happen"
     },
-    "commitment": {
+    "commitmentO": {
       "action": "produce",
+      "outputOf": processId,
+      "provider": tempProviderAgentId,
+      "receiver": tempReceiverAgentId,
       "note": "I'll make the thing happen"
     },
-    "event": {
+    "eventO": {
       "action": "produce",
+      "outputOf": processId,
+      "provider": tempProviderAgentId,
+      "receiver": tempReceiverAgentId,
       "note": "hooray, the thing happened!"
     },
   })
-
   await s.consistency()
 
-  t.ok(cResp.data.createIntent.intent.id, "intent created OK")
-  t.ok(cResp.data.createCommitment.commitment.id, "commitment created OK")
-  t.ok(cResp.data.createEconomicEvent.economicEvent.id, "event created OK")
+  t.ok(cResp.data.inputIntent.intent.id, "input intent created OK")
+  t.ok(cResp.data.inputCommitment.commitment.id, "input commitment created OK")
+  t.ok(cResp.data.inputEvent.economicEvent.id, "input event created OK")
+  t.ok(cResp.data.outputIntent.intent.id, "output intent created OK")
+  t.ok(cResp.data.outputCommitment.commitment.id, "output commitment created OK")
+  t.ok(cResp.data.outputEvent.economicEvent.id, "output event created OK")
 
 })
 
