5393030eba19b58c698a2dcf43eb1e3386e41355
pospi <pospi@spadgos.com>
2019-10-31 11:47:27 +1000

    upgrade graphQL adapter to allow injecting per-request connection URIs

--- modules/vf-graphql-holochain/connection.ts
+++ modules/vf-graphql-holochain/connection.ts
@@ -10,13 +10,29 @@
 
 import { connect } from '@holochain/hc-web-client'
 
+let DEFAULT_CONNECTION_URI = process.env.REACT_APP_HC_CONN_URL ? { url: process.env.REACT_APP_HC_CONN_URL } : undefined
 const CONNECTION_CACHE = {}
 
+/**
+ * For use by external scripts which need to init multiple connections.
+ * You can call this method prior to triggering a `zomeFunction` to ensure
+ * that the call will hit the specified websocket URI.
+ */
+export function setConnectionURI (url: string): void {
+  DEFAULT_CONNECTION_URI = { url }
+}
+
+/**
+ * Inits a connection for the given websocket URI. If none is provided, a default
+ * connection is attempted either by `REACT_APP_HC_CONN_URL` environment variable
+ * or the built-in Holochain conductor resolution if the app is run from within
+ * a Holochain conductor.
+ */
 const BASE_CONNECTION = (socketURI = undefined) => {
   if (!socketURI) {
-    socketURI = process.env.REACT_APP_HC_CONN_URL ? { url: process.env.REACT_APP_HC_CONN_URL } : undefined
+    socketURI = DEFAULT_CONNECTION_URI
   }
-  const connId = socketURI || '__default__'
+  const connId = socketURI ? socketURI.url : '__default__'
 
   if (CONNECTION_CACHE[connId]) {
     return CONNECTION_CACHE[connId]
@@ -37,6 +53,9 @@ export interface ZomeFnOpts {
   resultParser?: (resp: any) => any
 }
 
+/**
+ * Higher-order function to generate async functions for calling zome RPC methods
+ */
 export const zomeFunction = (instance, zome, fn, socketURI = undefined) => async (args, opts: ZomeFnOpts = {}) => {
   const { callZome } = await BASE_CONNECTION(socketURI)
   const zomeCall = callZome(instance, zome, fn)
