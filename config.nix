d6a01b907fb8be0e0ab8a37060ed25bbb97f3347
pospi <pospi@spadgos.com>
2019-11-14 15:18:19 +1000

    upgrade platform and switch to official package manager
    (downloads much faster than the whole git history)

--- config.nix
+++ config.nix
@@ -14,14 +14,14 @@
 
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "v0.0.39";
+   ref = "v0.0.47";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
    #       the sha here changes (the sha is the cache key for downloads)
    # note: to get a new sha, get nix to try and download a bad sha
    #       it will complain and tell you the right sha
-   sha256 = "1c8jk9cmpayjy7bndymx5c7wshwgd7a2xqda4lvp2y7ds72i8jhz";
+   sha256 = "0fyal8y02abp71y0l3szcbc9d0r89ph5yk3d6maw28as78mw7v23";
 
 
    # the github owner of the holonix repo
