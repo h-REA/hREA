68463eafe84b8ff6c0ed61f9dc3d2552873622b7
pospi <pospi@spadgos.com>
2019-11-19 16:30:48 +1000

    update QV field names to match OM ontology per VF0.4 release

--- lib/vf_core/src/measurement.rs
+++ lib/vf_core/src/measurement.rs
@@ -11,40 +11,40 @@ pub struct Unit {
 #[serde(rename_all = "camelCase")]
 pub struct QuantityValue {
     // :TODO: https://users.rust-lang.org/t/currency-in-rust/890/9 ?
-    numeric_value: f64,     // :NOTE: uses https://en.wikipedia.org/wiki/IEEE_754 for math
+    has_numerical_value: f64,     // :NOTE: uses https://en.wikipedia.org/wiki/IEEE_754 for math
     #[serde(default)]
-    unit: Option<UnitAddress>,
+    has_unit: Option<UnitAddress>,
 }
 
 impl<'a> QuantityValue {
-    pub fn new(numeric_value: f64, unit: Option<UnitAddress>) -> QuantityValue {
+    pub fn new(has_numerical_value: f64, has_unit: Option<UnitAddress>) -> QuantityValue {
         QuantityValue {
-            numeric_value,
-            unit,
+            has_numerical_value,
+            has_unit,
         }
     }
 
     pub fn get_unit(&'a self) -> Option<UnitAddress> {
-        self.unit.to_owned()
+        self.has_unit.to_owned()
     }
 }
 
 pub fn add(q1: QuantityValue, q2: QuantityValue) -> QuantityValue {
-    if q1.unit != q2.unit {
+    if q1.has_unit != q2.has_unit {
         panic!("Unimplemented! Need to enable unit conversions in QuantityValue math");
     }
     QuantityValue {
-        numeric_value: q1.numeric_value + q2.numeric_value,
-        unit: q1.unit,
+        has_numerical_value: q1.has_numerical_value + q2.has_numerical_value,
+        has_unit: q1.has_unit,
     }
 }
 
 pub fn subtract(q1: QuantityValue, q2: QuantityValue) -> QuantityValue {
-    if q1.unit != q2.unit {
+    if q1.has_unit != q2.has_unit {
         panic!("Unimplemented! Need to enable unit conversions in QuantityValue math");
     }
     QuantityValue {
-        numeric_value: q1.numeric_value - q2.numeric_value,
-        unit: q1.unit,
+        has_numerical_value: q1.has_numerical_value - q2.has_numerical_value,
+        has_unit: q1.has_unit,
     }
 }
