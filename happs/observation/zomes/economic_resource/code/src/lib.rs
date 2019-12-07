6fe037bc420edcde993f0d80a72be509ccf2aaf4
tommycp3 <thomasmillerprivate@gmail.com>
2019-12-07 12:05:39 +1000

    convert economic_resource to rust_proc_macro format

--- happs/observation/zomes/economic_resource/code/src/lib.rs
+++ happs/observation/zomes/economic_resource/code/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(proc_macro_hygiene)]
 // :TODO: documentation
 
 #[macro_use]
@@ -16,8 +17,9 @@ use hdk::{
     error::ZomeApiResult,
     holochain_persistence_api::cas::content::Address,
     holochain_core_types::dna::entry_types::Sharing,
-    holochain_json_api::{ json::JsonString, error::JsonError },
+    
 };
+use hdk_proc_macros::zome;
 
 use vf_observation::type_aliases::{
     ResourceAddress,
@@ -45,119 +47,113 @@ use vf_observation::identifiers::{
 
 // Zome entry type wrappers
 
-fn resource_entry_def() -> ValidatingEntryType {
-    entry!(
-        name: RESOURCE_ENTRY_TYPE,
-        description: "A resource which is useful to people or the ecosystem.",
-        sharing: Sharing::Public,
-        validation_package: || {
-            hdk::ValidationPackageDefinition::Entry
-        },
-        validation: |_validation_data: hdk::EntryValidationData<Entry>| {
-            Ok(())
-        }
-    )
-}
+#[zome]
+mod rea_economic_resource_zome {
 
-fn resource_base_entry_def() -> ValidatingEntryType {
-    entry!(
-        name: RESOURCE_BASE_ENTRY_TYPE,
-        description: "Base anchor for initial resource addresses to provide lookup functionality",
-        sharing: Sharing::Public,
-        validation_package: || {
-            hdk::ValidationPackageDefinition::Entry
-        },
-        validation: |_validation_data: hdk::EntryValidationData<Address>| {
-            Ok(())
-        },
-        links: [
-            to!(
-                RESOURCE_ENTRY_TYPE,
-                link_type: RESOURCE_INITIAL_ENTRY_LINK_TYPE,
-                validation_package: || {
-                    hdk::ValidationPackageDefinition::Entry
-                },
-                validation: | _validation_data: hdk::LinkValidationData| {
-                    Ok(())
-                }
-            ),
-            to!(
-                RESOURCE_BASE_ENTRY_TYPE,
-                link_type: RESOURCE_CONTAINS_LINK_TYPE,
-                validation_package: || {
-                    hdk::ValidationPackageDefinition::Entry
-                },
-                validation: | _validation_data: hdk::LinkValidationData| {
-                    Ok(())
-                }
-            ),
-            to!(
-                RESOURCE_BASE_ENTRY_TYPE,
-                link_type: RESOURCE_CONTAINED_IN_LINK_TYPE,
-                validation_package: || {
-                    hdk::ValidationPackageDefinition::Entry
-                },
-                validation: | _validation_data: hdk::LinkValidationData| {
-                    Ok(())
-                }
-            ),
-            to!(
-                EVENT_BASE_ENTRY_TYPE,
-                link_type: RESOURCE_AFFECTED_BY_EVENT_LINK_TYPE,
-                validation_package: || {
-                    hdk::ValidationPackageDefinition::Entry
-                },
-                validation: | _validation_data: hdk::LinkValidationData| {
-                    Ok(())
-                }
-            )
-        ]
-    )
-}
-
-// Zome definition
-
-define_zome! {
-    entries: [
-        resource_entry_def(),
-        resource_base_entry_def()
-    ]
-
-    init: || {
+    #[init]
+    fn init() {
         Ok(())
     }
 
-    validate_agent: |validation_data : EntryValidationData::<AgentId>| {
+    #[validate_agent]
+    pub fn validate_agent(validation_data: EntryValidationData::<AgentId>) {
         Ok(())
+
     }
 
-    receive: |from, payload| {
-      format!("Received: {} from {}", payload, from)
+    #[entry_def]
+    fn resource_entry_def() -> ValidatingEntryType {
+        entry!(
+            name: RESOURCE_ENTRY_TYPE,
+            description: "A resource which is useful to people or the ecosystem.",
+            sharing: Sharing::Public,
+            validation_package: || {
+                hdk::ValidationPackageDefinition::Entry
+            },
+            validation: |_validation_data: hdk::EntryValidationData<Entry>| {
+                Ok(())
+            }
+        )
+
     }
 
-    functions: [
-        get_resource: {
-            inputs: |address: ResourceAddress|,
-            outputs: |result: ZomeApiResult<ResponseData>|,
-            handler: receive_get_economic_resource
-        }
-        update_resource: {
-            inputs: |resource: UpdateRequest|,
-            outputs: |result: ZomeApiResult<ResponseData>|,
-            handler: receive_update_economic_resource
-        }
-        query_resources: {
-            inputs: |params: QueryParams|,
-            outputs: |result: ZomeApiResult<Vec<ResponseData>>|,
-            handler: receive_query_economic_resources
-        }
-    ]
-
-    traits: {
-        hc_public [
-            get_resource,
-            update_resource,
-            query_resources
-        ]
+    #[entry_def]
+    fn resource_base_entry_def() -> ValidatingEntryType {
+        entry!(
+            name: RESOURCE_BASE_ENTRY_TYPE,
+            description: "Base anchor for initial resource addresses to provide lookup functionality",
+            sharing: Sharing::Public,
+            validation_package: || {
+                hdk::ValidationPackageDefinition::Entry
+            },
+            validation: |_validation_data: hdk::EntryValidationData<Address>| {
+                Ok(())
+            },
+            links: [
+                to!(
+                    RESOURCE_ENTRY_TYPE,
+                    link_type: RESOURCE_INITIAL_ENTRY_LINK_TYPE,
+                    validation_package: || {
+                        hdk::ValidationPackageDefinition::Entry
+                    },
+                    validation: | _validation_data: hdk::LinkValidationData| {
+                        Ok(())
+                    }
+                ),
+                to!(
+                    RESOURCE_BASE_ENTRY_TYPE,
+                    link_type: RESOURCE_CONTAINS_LINK_TYPE,
+                    validation_package: || {
+                        hdk::ValidationPackageDefinition::Entry
+                    },
+                    validation: | _validation_data: hdk::LinkValidationData| {
+                        Ok(())
+                    }
+                ),
+                to!(
+                    RESOURCE_BASE_ENTRY_TYPE,
+                    link_type: RESOURCE_CONTAINED_IN_LINK_TYPE,
+                    validation_package: || {
+                        hdk::ValidationPackageDefinition::Entry
+                    },
+                    validation: | _validation_data: hdk::LinkValidationData| {
+                        Ok(())
+                    }
+                ),
+                to!(
+                    EVENT_BASE_ENTRY_TYPE,
+                    link_type: RESOURCE_AFFECTED_BY_EVENT_LINK_TYPE,
+                    validation_package: || {
+                        hdk::ValidationPackageDefinition::Entry
+                    },
+                    validation: | _validation_data: hdk::LinkValidationData| {
+                        Ok(())
+                    }
+                )
+            ]
+        )
     }
+
+    #[zome_fn("hc_public")]
+    fn get_resource(address: ResourceAddress) -> ZomeApiResult<ResponseData> {
+        receive_get_economic_resource(address)
+    }
+
+    #[zome_fn("hc_public")]
+    fn update_resource(resource: UpdateRequest) -> ZomeApiResult<ResponseData> {
+        receive_update_economic_resource(resource)
+    }
+
+
+    #[zome_fn("hc_public")]
+    fn query_resources(params: QueryParams) -> ZomeApiResult<Vec<ResponseData>> {
+        receive_query_economic_resources(params)
+    }
+
+
+    // :TODO:
+    // receive: |from, payload| {
+    //     format!("Received: {} from {}", payload, from)
+    // }
+
 }
