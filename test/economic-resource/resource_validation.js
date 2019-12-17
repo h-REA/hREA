828878555e2603db53262d026b1fb70b02daf6a1
pospi <pospi@spadgos.com>
2019-12-17 15:17:57 +1000

    more tests for resource validation logic

--- test/economic-resource/resource_validation.js
+++ test/economic-resource/resource_validation.js
@@ -19,7 +19,7 @@ const testEventProps = {
   hasPointInTime: '2019-11-19T04:29:55.056Z',
 }
 
-runner.registerScenario('EconomicResource record validation', async (s, t) => {
+runner.registerScenario('EconomicResource classification fields validation', async (s, t) => {
   const { alice } = await s.players({ alice: config }, true)
   alice.graphQL = buildGraphQL(alice)
 
@@ -48,6 +48,84 @@ runner.registerScenario('EconomicResource record validation', async (s, t) => {
 
   t.equal(resp.errors.length, 1, 'creating resource without ontological bindings is an error')
   t.notEqual(-1, resp.errors[0].message.indexOf('EconomicResource must have either a specification or classification'), 'correct error reported')
+
+  resp = await alice.graphQL(`
+    mutation($e: EconomicEventCreateParams!, $r: EconomicResourceCreateParams) {
+      createEconomicEvent(event: $e, newInventoriedResource: $r) {
+        economicEvent {
+          id
+        }
+        economicResource {
+          id
+        }
+      }
+    }
+  `, {
+    e: {
+      action: 'raise',
+      resourceQuantity: { hasNumericalValue: 1, hasUnit: 'kg' },
+      resourceClassifiedAs: ['some-classification-url'],
+      ...testEventProps,
+    },
+    r: {
+      name: 'good resource with ontological distinction capability',
+    },
+  })
+  await s.consistency()
+
+  t.ok(resp.data.createEconomicEvent.economicEvent.id, 'creating resource with classification is OK')
+
+  resp = await alice.graphQL(`
+    mutation($e: EconomicEventCreateParams!, $r: EconomicResourceCreateParams) {
+      createEconomicEvent(event: $e, newInventoriedResource: $r) {
+        economicEvent {
+          id
+        }
+        economicResource {
+          id
+        }
+      }
+    }
+  `, {
+    e: {
+      action: 'raise',
+      resourceQuantity: { hasNumericalValue: 1, hasUnit: 'kg' },
+      resourceConformsTo: 'some-specification-id',
+      ...testEventProps,
+    },
+    r: {
+      name: 'good resource with ontological distinction capability',
+    },
+  })
+  await s.consistency()
+
+  t.ok(resp.data.createEconomicEvent.economicEvent.id, 'creating resource with event specification is OK')
+
+  resp = await alice.graphQL(`
+    mutation($e: EconomicEventCreateParams!, $r: EconomicResourceCreateParams) {
+      createEconomicEvent(event: $e, newInventoriedResource: $r) {
+        economicEvent {
+          id
+        }
+        economicResource {
+          id
+        }
+      }
+    }
+  `, {
+    e: {
+      action: 'raise',
+      resourceQuantity: { hasNumericalValue: 1, hasUnit: 'kg' },
+      ...testEventProps,
+    },
+    r: {
+      name: 'good resource with ontological distinction capability',
+      conformsTo: 'some-specification-id',
+    },
+  })
+  await s.consistency()
+
+  t.ok(resp.data.createEconomicEvent.economicEvent.id, 'creating resource with resource specification is OK')
 })
 
 runner.run()
