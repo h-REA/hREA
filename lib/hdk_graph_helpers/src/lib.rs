e084f3ca82bcc87fab77e27e742dfb923e62f9ae
pospi <pospi@spadgos.com>
2020-01-04 17:12:52 +1000

    clean up exports for hdk_graph_helpers, hide internal aliases

--- lib/hdk_graph_helpers/src/lib.rs
+++ lib/hdk_graph_helpers/src/lib.rs
@@ -7,30 +7,34 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate hdk;
 
+// dependencies
+
 mod internals;
-pub mod type_wrappers;
+
+mod entry_helpers;
+mod key_helpers;
+mod local_index_helpers;
+mod remote_index_helpers;
+mod record_helpers;
+mod link_helpers;
+mod rpc_helpers;
+
+// API interfaces
+
+pub mod type_wrappers;  // :TODO: make private once unit_requests.rs dependency lifted
 pub mod maybe_undefined;
+pub use maybe_undefined::MaybeUndefined as MaybeUndefined;
 pub mod record_interface;
 
-pub mod entry_helpers;
-pub mod key_helpers;
-pub mod local_index_helpers;
-pub mod remote_index_helpers;
-pub mod record_helpers;
-pub mod link_helpers;
-pub mod rpc_helpers;
+// helper functions API
 
-// submodule exports
-
-pub use maybe_undefined::MaybeUndefined as MaybeUndefined;
-
-use entry_helpers as entries;
-pub use key_helpers as keys;
-pub use local_index_helpers as local_indexes;
-pub use remote_index_helpers as remote_indexes;
-pub use record_helpers as records;
-pub use link_helpers as links;
-pub use rpc_helpers as rpc;
+pub mod entries { pub use crate::entry_helpers::*; }
+pub mod links { pub use crate::link_helpers::*; }
+pub mod keys { pub use crate::key_helpers::*; }
+pub mod local_indexes { pub use crate::local_index_helpers::*; }
+pub mod remote_indexes { pub use crate::remote_index_helpers::*; }
+pub mod rpc { pub use crate::rpc_helpers::*; }
+pub mod records { pub use crate::record_helpers::*; }
 
 pub mod identifiers {
     pub const RECORD_INITIAL_ENTRY_LINK_TAG: &str = "initial_entry";
