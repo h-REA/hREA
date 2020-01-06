e464053fdaa06edc41f39123007b72f1bc55f723
tommycp3 <thomasmillerprivate@gmail.com>
2020-01-06 21:27:42 +0800

    pretty up the code

--- modules/vf-graphql-holochain/resolvers/economicResource.ts
+++ modules/vf-graphql-holochain/resolvers/economicResource.ts
@@ -7,7 +7,6 @@
 
 import { zomeFunction } from '../connection'
 
-// 1. Import the GraphQL shema for ResourceSpecification
 import {
   EconomicResource,
   ResourceSpecification,
@@ -20,8 +19,6 @@ const readResources = zomeFunction('observation', 'economic_resource', 'query_re
 const readUnit = zomeFunction('specification', 'unit', 'get_unit')
 const readProcessSpecification = zomeFunction('specification', 'process_specification', 'get_process_specification')
 const readAction = zomeFunction('specification', 'action', 'get_action')
-
-//2. make reference to zome function that reads the ResourceSpecification
 const readResourceSpecification = zomeFunction('specification', 'resource_specification', 'get_resource_specification')
 
 export const containedIn = async (record: EconomicResource): Promise<EconomicResource> => {
@@ -32,19 +29,18 @@ export const contains = async (record: EconomicResource): Promise<[EconomicResou
   return (await readResources({ params: { containedIn: record.id } })).map(({ economicResource }) => economicResource)
 }
 
-//3. export resolver function
 export const conformsTo = async (record: EconomicResource): Promise<ResourceSpecification> => {
   return (await readResourceSpecification({ address: record.conformsTo})).resourceSpecification
 }
 
-export const unitOfEffort = async(record: EconomicResource): Promise<Unit> => {
+export const unitOfEffort = async (record: EconomicResource): Promise<Unit> => {
   return (await readUnit({ address: record.unitOfEffort })).unit
 }
 
-export const stage = async(record: EconomicResource): Promise<ProcessSpecification> => {
+export const stage = async (record: EconomicResource): Promise<ProcessSpecification> => {
   return (await readProcessSpecification({ address: record.stage })).processSpecification
 }
 
 export const state = async (record: EconomicResource): Promise<Action> => {
-  return (await readAction({ address: record.state}))
+  return (await readAction({ address: record.state }))
 }
