cddd97556f0950081963cb0441d1e8d788f27a24
tommycp3 <thomasmillerprivate@gmail.com>
2020-01-06 17:18:35 +1000

    implement resolver for EconomicResource.conformsTo

--- modules/vf-graphql-holochain/resolvers/economicResource.ts
+++ modules/vf-graphql-holochain/resolvers/economicResource.ts
@@ -7,11 +7,16 @@
 
 import { zomeFunction } from '../connection'
 
+// 1. Import the GraphQL shema for ResourceSpecification
 import {
   EconomicResource,
+  ResourceSpecification
 } from '@valueflows/vf-graphql'
 
 const readResources = zomeFunction('observation', 'economic_resource', 'query_resources')
+
+//2. make reference to zome function that reads the ResourceSpecification
+const readResourceSpecification = zomeFunction('specification', 'resource_specification', 'get_resource_specification')
 
 export const containedIn = async (record: EconomicResource): Promise<EconomicResource> => {
   return (await readResources({ params: { contains: record.id } })).pop()['economicResource']
@@ -19,4 +24,9 @@ export const containedIn = async (record: EconomicResource): Promise<EconomicRes
 
 export const contains = async (record: EconomicResource): Promise<[EconomicResource]> => {
   return (await readResources({ params: { containedIn: record.id } })).map(({ economicResource }) => economicResource)
+}
+
+//3. export resolver function
+export const conformsTo = async (record: EconomicResource): Promise<ResourceSpecification> => {
+  return (await readResourceSpecification({ address: record.conformsTo})).resourceSpecification
 }
