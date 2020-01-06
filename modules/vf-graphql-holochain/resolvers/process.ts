a7f77005e2655b33e0123c77a32c6a637e61f7be
tommycp3 <thomasmillerprivate@gmail.com>
2020-01-06 21:09:15 +0800

    implement resolver for Process.basedOn

--- modules/vf-graphql-holochain/resolvers/process.ts
+++ modules/vf-graphql-holochain/resolvers/process.ts
@@ -13,12 +13,14 @@ import {
   EconomicEvent,
   Commitment,
   Intent,
+  ProcessSpecification
 } from '@valueflows/vf-graphql'
 
 // :TODO: how to inject DNA identifier?
 const readEvents = zomeFunction('observation', 'economic_event', 'query_events')
 const readCommitments = zomeFunction('planning', 'commitment', 'query_commitments')
 const readIntents = zomeFunction('planning', 'intent', 'query_intents')
+const readProcessBasedOn = zomeFunction('specification', 'process_specification', 'get_process_specification')
 
 export const inputs = injectTypename('EconomicEvent', async (record: Process): Promise<[EconomicEvent]> => {
   return (await readEvents({ params: { inputOf: record.id } })).map(({ economicEvent }) => economicEvent)
@@ -42,4 +44,8 @@ export const intendedInputs = async (record: Process): Promise<[Intent]> => {
 
 export const intendedOutputs = async (record: Process): Promise<[Intent]> => {
   return (await readIntents({ params: { outputOf: record.id } })).map(({ intent }) => intent)
+}
+
+export const basedOn = async (record: Process): Promise<ProcessSpecification> => {
+  return (await readProcessBasedOn({ address: record.basedOn })).processSpecification
 }
