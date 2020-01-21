90beefdb333c8731a0c18fd81d3d560bcadf41a9
pospi <pospi@spadgos.com>
2020-01-21 21:34:58 +1000

    cleanup vf_core dependencies to reduce coupling on hdk

--- lib/vf_core/src/lib.rs
+++ lib/vf_core/src/lib.rs
@@ -2,7 +2,9 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate holochain_json_api;
 extern crate holochain_json_derive;
+extern crate holochain_persistence_api;
 
 // #[macro_use]
 pub mod vf_record;
