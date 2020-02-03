d43b4d448e3ed72440f4a8c2a73af09d863a99f6
JhonatanHern <jhonatanhernandez998@gmail.com>
2020-02-03 13:32:22 +0000

    fixed constant values

--- lib/rea_proposal/storage_consts/src/lib.rs
+++ lib/rea_proposal/storage_consts/src/lib.rs
@@ -9,5 +9,5 @@ pub const PROPOSAL_ENTRY_TYPE: &str = "vf_proposal";
 pub const PROPOSAL_BASE_ENTRY_TYPE: &str = "vf_proposal_baseurl";
 pub const PROPOSAL_INITIAL_ENTRY_LINK_TYPE: &str = "vf_proposal_entry";
 
-pub const PROPOSAL_PUBLISHES_LINK_TYPE: &str = "vf_proposal_published_link_type";
-pub const PROPOSAL_PUBLISHES_LINK_TAG: &str = "vf_proposal_published_link_tag";
+pub const PROPOSAL_PUBLISHES_LINK_TYPE: &str = "vf_proposal_publishes";
+pub const PROPOSAL_PUBLISHES_LINK_TAG: &str = "publishes";
