7962987cd4efac3d33d18b294c856aa31998c7d0
JhonatanHern <jhonatanhernandez998@gmail.com>
2020-02-03 13:26:46 +0000

    fixed constant values

--- lib/rea_proposed_intent/storage_consts/src/lib.rs
+++ lib/rea_proposed_intent/storage_consts/src/lib.rs
@@ -9,10 +9,10 @@ pub const PROPOSED_INTENT_ENTRY_TYPE: &str = "vf_proposed_intent";
 pub const PROPOSED_INTENT_BASE_ENTRY_TYPE: &str = "vf_proposed_intent_baseurl";
 pub const PROPOSED_INTENT_INITIAL_ENTRY_LINK_TYPE: &str = "vf_proposed_intent_entry";
 
-pub const PROPOSED_INTENT_PUBLISHED_IN_LINK_TYPE: &str = "vf_proposed_intent_published_in_link_type";
-pub const PROPOSED_INTENT_PUBLISHED_IN_LINK_TAG: &str = "vf_proposed_intent_published_in_link_tag";
+pub const PROPOSED_INTENT_PUBLISHED_IN_LINK_TYPE: &str = "vf_proposed_intent_published_in";
+pub const PROPOSED_INTENT_PUBLISHED_IN_LINK_TAG: &str = "published_in";
 
-pub const PROPOSED_INTENT_PUBLISHES_LINK_TYPE: &str = "vf_proposed_intent_publishes_link_type";
-pub const PROPOSED_INTENT_PUBLISHES_LINK_TAG: &str = "vf_proposed_intent_publishes_link_tag";
+pub const PROPOSED_INTENT_PUBLISHES_LINK_TYPE: &str = "vf_proposed_intent_publishes";
+pub const PROPOSED_INTENT_PUBLISHES_LINK_TAG: &str = "publishes";
 
 pub const BRIDGED_PLANNING_DHT: &str = "vf_planning";
--- lib/rea_proposed_to/storage_consts/src/lib.rs
+++ lib/rea_proposed_to/storage_consts/src/lib.rs
@@ -8,5 +8,6 @@
 pub const PROPOSED_TO_ENTRY_TYPE: &str = "vf_proposed_to";
 pub const PROPOSED_TO_BASE_ENTRY_TYPE: &str = "vf_proposed_to_id";
 pub const PROPOSED_TO_INITIAL_ENTRY_LINK_TYPE: &str = "vf_proposed_to_entry";
-pub const PROPOSED_TO_PROPOSED_LINK_TYPE: &str = "proposed_to_proposed_link_type";
-pub const PROPOSED_TO_PROPOSED_LINK_TAG: &str = "proposed_to_proposed_link_tag";
+
+pub const PROPOSED_TO_PROPOSED_LINK_TYPE: &str = "proposed_to_proposed";
+pub const PROPOSED_TO_PROPOSED_LINK_TAG: &str = "proposed";
