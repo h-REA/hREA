bfc1eeac2a2d47d3ad62193f83c9c6d96646be2c
JhonatanHern <jhonatanhernandez998@gmail.com>
2020-02-27 21:45:30 +0000

    added Intent link to ProposedIntent resolver

--- modules/vf-graphql-holochain/resolvers/proposedIntent.ts
+++ modules/vf-graphql-holochain/resolvers/proposedIntent.ts
@@ -9,12 +9,18 @@ import { zomeFunction } from '../connection'
 
 import {
   Proposal,
+  Intent,
   ProposedIntent,
 } from '@valueflows/vf-graphql'
 
 // :TODO: how to inject DNA identifier?
 const readProposal = zomeFunction('proposal', 'proposal', 'get_proposal')
+const readIntent = zomeFunction('planning', 'intent', 'get_intent')
 
 export const publishedIn = async (record: ProposedIntent): Promise<Proposal> => {
   return (await readProposal({address:record.publishedIn})).proposal
+}
+
+export const publishes = async (record: ProposedIntent): Promise<Intent> => {
+  return (await readIntent({address:record.publishes})).intent
 }
