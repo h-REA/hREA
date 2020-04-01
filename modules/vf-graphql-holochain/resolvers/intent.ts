3f669a06d8898c0a20aa72ddf03319be2c24255f
JhonatanHern <jhonatanhernandez998@gmail.com>
2020-04-01 19:44:55 +0000

    added missing resolver to Intent and vf-graphql updated to 0.7.1

--- modules/vf-graphql-holochain/resolvers/intent.ts
+++ modules/vf-graphql-holochain/resolvers/intent.ts
@@ -12,12 +12,14 @@ import {
   Satisfaction,
   Process,
   ResourceSpecification,
+  ProposedIntent,
   Action
 } from '@valueflows/vf-graphql'
 
 // :TODO: how to inject DNA identifier?
 const readSatisfactions = zomeFunction('planning', 'satisfaction', 'query_satisfactions')
 const readProcesses = zomeFunction('observation', 'process', 'query_processes')
+const readProposedIntent = zomeFunction('proposal', 'proposed_intent', 'get_proposed_intent')
 const readResourceSpecification = zomeFunction('specification', 'resource_specification', 'get_resource_specification')
 const readAction = zomeFunction('specification', 'action', 'get_action')
 
@@ -31,6 +33,12 @@ export const outputOf = async (record: Intent): Promise<Process[]> => {
 
 export const satisfiedBy = async (record: Intent): Promise<Satisfaction[]> => {
   return (await readSatisfactions({ params: { satisfies: record.id } })).map(({ satisfaction }) => satisfaction)
+}
+
+const extractProposedIntent = (data): ProposedIntent => data.proposedIntent
+
+export const publishedIn = async (record: Intent): Promise<ProposedIntent[]> => {
+  return (await Promise.all((record.publishedIn || []).map((address)=>readProposedIntent({address})))).map(extractProposedIntent)
 }
 
 export const resourceConformsTo = async (record: Intent): Promise<ResourceSpecification> => {
