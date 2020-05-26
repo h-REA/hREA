e9eae2065516042ad250add1155813ab9572bae3
pospi <pospi@spadgos.com>
2020-05-26 17:24:32 +1000

    happy-path tests for agent queries

--- test/agent/test_agent_core.js
+++ test/agent/test_agent_core.js
@@ -14,14 +14,57 @@ const config = buildConfig({
 
 runner.registerScenario('REA economic agent functionality', async (s, t) => {
   const alice = await buildPlayer(s, 'alice', config)
+  const aliceAddr = alice.instance('agent').agentAddress
 
-  const agentData = await alice.graphQL(`{
+  let res = await alice.graphQL(`{
     myAgent {
       id
+      name
     }
   }`)
 
-  t.ok(agentData.data.myAgent.id, 'can retrieve own agent ID')
+  // :TODO: register personas & proof sharing with the network
+
+  t.ok(res.data.myAgent.id, 'agent A can retrieve own agent ID')
+  t.ok(res.data.myAgent.name, 'agent A can retrieve own agent name')
+
+  res = await alice.graphQL(`{
+    allAgents {
+      id
+      name
+    }
+  }`)
+  t.equal(res.data.allAgents.length, 1, 'can load agents list')
+  t.equal(res.data.allAgents[0].id, aliceAddr, 'own agent ID returned in list')
+
+  const bob = await buildPlayer(s, 'bob', config)
+  const bobAddr = bob.instance('agents').agentAddress
+
+  res = await bob.graphQL(`{
+    myAgent {
+      id
+      name
+    }
+  }`)
+  t.ok(res.data.myAgent.id, 'agent B can retrieve own agent ID')
+
+  res = await alice.graphQL(`{
+    allAgents {
+      id
+      name
+    }
+  }`)
+  t.equal(res.data.allAgents.length, 2, 'can load agents list after second agent joins')
+  t.equal(res.data.allAgents[0].id, aliceAddr, 'own agent ID returned in list')
+  t.equal(res.data.allAgents[1].id, aliceAddr, 'new agent ID returned in list')
+
+  res = await alice.graphQL(`{
+    agent(id: "${bobAddr}") {
+      id
+      name
+    }
+  }`)
+  t.equal(res.data.agent.id, bobAddr, 'can load other agent details by ID')
 })
 
 runner.run()
