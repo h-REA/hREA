9c8b35bb5f9f90a3c52ab9b908055d0d2f8024db
pospi <pospi@spadgos.com>
2020-05-27 18:56:19 +1000

    allow optional DNA name override when generating DNA configs

--- test/init.js
+++ test/init.js
@@ -27,7 +27,7 @@ process.on('unhandledRejection', error => {
 })
 
 // DNA loader, to be used with `buildTestScenario` when constructing DNAs for testing
-const getDNA = ((dnas) => (path) => (Config.dna(dnas[path], path)))({
+const getDNA = ((dnas) => (path, name) => (Config.dna(dnas[path], name || path)))({
   'agent': path.resolve(__dirname, '../happs/agent/dist/agent.dna.json'),
   'specification': path.resolve(__dirname, '../happs/specification/dist/specification.dna.json'),
   'observation': path.resolve(__dirname, '../happs/observation/dist/observation.dna.json'),
