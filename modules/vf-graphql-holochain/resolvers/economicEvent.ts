dffc8026a23e1f560b06a17761bbeaa2994690d1
pospi <pospi@spadgos.com>
2020-05-28 18:09:38 +1000

    fix types

--- modules/vf-graphql-holochain/resolvers/economicEvent.ts
+++ modules/vf-graphql-holochain/resolvers/economicEvent.ts
@@ -23,19 +23,19 @@ const readProcesses = zomeFunction('observation', 'process', 'query_processes')
 const readAction = zomeFunction('specification', 'action', 'get_action')
 const readResourceSpecification = zomeFunction('specification', 'resource_specification', 'get_resource_specification')
 
-export const inputOf = async (record: EconomicEvent): Promise<[Process]> => {
+export const inputOf = async (record: EconomicEvent): Promise<Process[]> => {
   return (await readProcesses({ params: { inputs: record.id } })).pop()['process']
 }
 
-export const outputOf = async (record: EconomicEvent): Promise<[Process]> => {
+export const outputOf = async (record: EconomicEvent): Promise<Process[]> => {
   return (await readProcesses({ params: { outputs: record.id } })).pop()['process']
 }
 
-export const fulfills = async (record: EconomicEvent): Promise<[Fulfillment]> => {
+export const fulfills = async (record: EconomicEvent): Promise<Fulfillment[]> => {
   return (await readFulfillments({ params: { fulfilledBy: record.id } })).map(({ fulfillment }) => fulfillment)
 }
 
-export const satisfies = async (record: EconomicEvent): Promise<[Satisfaction]> => {
+export const satisfies = async (record: EconomicEvent): Promise<Satisfaction[]> => {
   return (await readSatisfactions({ params: { satisfiedBy: record.id } })).map(({ satisfaction }) => satisfaction)
 }
 
