a8ed5c1bcb8e56b7833ebfb7a3e542a531b407c7
pospi <pospi@spadgos.com>
2020-10-27 13:27:50 +1000

    update vf_core package imports

--- lib/vf_core/src/lib.rs
+++ lib/vf_core/src/lib.rs
@@ -1,10 +1,2 @@
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-extern crate holochain_json_api;
-extern crate holochain_json_derive;
-extern crate holochain_persistence_api;
-
 pub mod type_aliases;
 pub mod measurement;
