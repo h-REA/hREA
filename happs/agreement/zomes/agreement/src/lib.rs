d132b9e3a8864eb4cb7d9b8f73b56ee0d1d3db16
pospi <pospi@spadgos.com>
2021-02-06 16:32:55 +1000

    finalise Agreement zome, now compiles!

--- happs/agreement/zomes/agreement/src/lib.rs
+++ happs/agreement/zomes/agreement/src/lib.rs
@@ -11,45 +11,37 @@ use hdk3::prelude::*;
 
 use hc_zome_rea_agreement_rpc::*;
 use hc_zome_rea_agreement_lib::*;
-use hc_zome_rea_agreement_storage::*;
-
-entry_defs![Path::entry_def()];
-entry_interface!(
-    EntryStorage EntryDef {
-        id: "rea_agreement".into(),
-        visibility: EntryVisibility::Public,
-        crdt_type: CrdtType,
-        required_validations: 2.into(),
-        required_validation_type: RequiredValidationType::default(),
-    }
-);
 
 #[hdk_extern]
-fn init() {
-    Ok(())
-}
-
-#[hdk_extern]
-pub fn validate_agent(validation_data: EntryValidationData::<AgentId>) {
-    Ok(())
+fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
+    Ok(EntryDefsCallbackResult::from(vec![
+        Path::entry_def(),
+        EntryDef {
+            id: "rea_agreement".into(),
+            visibility: EntryVisibility::Public,
+            crdt_type: CrdtType,
+            required_validations: 2.into(),
+            required_validation_type: RequiredValidationType::default(),
+        }
+    ]))
 }
 
 #[hdk_extern]
 fn create_agreement(agreement: CreateRequest) -> ExternResult<ResponseData> {
-    receive_create_agreement(agreement)
+    Ok(receive_create_agreement(agreement)?)
 }
 
 #[hdk_extern]
 fn get_agreement(address: AgreementAddress) -> ExternResult<ResponseData> {
-    receive_get_agreement(address)
+    Ok(receive_get_agreement(address)?)
 }
 
 #[hdk_extern]
 fn update_agreement(agreement: UpdateRequest) -> ExternResult<ResponseData> {
-    receive_update_agreement(agreement)
+    Ok(receive_update_agreement(agreement)?)
 }
 
 #[hdk_extern]
 fn delete_agreement(address: HeaderHash) -> ExternResult<bool> {
-    receive_delete_agreement::<EntryStorage>(address)
+    Ok(receive_delete_agreement(address)?)
 }
