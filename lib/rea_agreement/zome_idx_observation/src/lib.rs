c11e2a64a1cf17a99f36f23ec62ec860bcbeed00
pospi <pospi@spadgos.com>
2021-03-12 13:34:15 +1000

    replace agreement foreign key index zomes with stubs for rethinking composable indexing logic

--- lib/rea_agreement/zome_idx_observation/src/lib.rs
+++ lib/rea_agreement/zome_idx_observation/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(proc_macro_hygiene)]
 /**
  * Holo-REA agreement index zome API definition
  *
@@ -6,62 +5,20 @@
  *
  * @package Holo-REA
  */
-extern crate serde;
-extern crate hdk;
-extern crate hdk_proc_macros;
-
 use hdk::prelude::*;
-use hdk_proc_macros::zome;
 
 use hc_zome_rea_economic_event_storage_consts::{ EVENT_BASE_ENTRY_TYPE, EVENT_REALIZATION_OF_LINK_TYPE };
 use hc_zome_rea_agreement_storage_consts::{ AGREEMENT_BASE_ENTRY_TYPE, AGREEMENT_EVENTS_LINK_TYPE };
 
-#[zome]
-mod rea_agreement_index_zome {
-    #[init]
-    fn init() {
-        Ok(())
-    }
+entry_defs![ Path::entry_def() ];
 
-    #[validate_agent]
-    pub fn validate_agent(validation_data: EntryValidationData::<AgentId>) {
-        Ok(())
-    }
-
-    #[entry_def]
-    fn index_base_entry_def() -> ValidatingEntryType {
-        entry!(
-            name: AGREEMENT_BASE_ENTRY_TYPE,
-            description: "Base anchor for external Agreement records to provide lookup functionality",
-            sharing: Sharing::Public,
-            validation_package: || {
-                hdk::ValidationPackageDefinition::Entry
-            },
-            validation: |_validation_data: hdk::EntryValidationData<Address>| {
-                Ok(())
-            },
-            links: [
-                to!(
-                    EVENT_BASE_ENTRY_TYPE,
-                    link_type: AGREEMENT_EVENTS_LINK_TYPE,
-                    validation_package: || {
-                        hdk::ValidationPackageDefinition::Entry
-                    },
-                    validation: | _validation_data: hdk::LinkValidationData| {
-                        Ok(())
-                    }
-                ),
-                from!(
-                    EVENT_BASE_ENTRY_TYPE,
-                    link_type: EVENT_REALIZATION_OF_LINK_TYPE,
-                    validation_package: || {
-                        hdk::ValidationPackageDefinition::Entry
-                    },
-                    validation: | _validation_data: hdk::LinkValidationData| {
-                        Ok(())
-                    }
-                )
-            ]
-        )
-    }
-}
+// :TODO:
+// Query EconomicEvents by event.realizationOf
+// Will require some thinking as to how to compose query functionality between pluggable zomes.
+// In the naive case we would handle event.realizationOf in this zome, commitment.clauseOf in a
+// separate "hc_zome_rea_agreement_index_planning" zome. Though it makes composability simpler,
+// this decomposition of indexes into separate zomes prevents any smart set-intersection logic
+// from being able to efficiently handle compound queries across these fields.
+//
+// Suggest some pattern for a pluggable "indexing" zome or DNA with further configuration that
+// can be used across various underlying `hdk::Path` indexing structures for combinatorial search.
--- lib/rea_agreement/zome_idx_planning/src/lib.rs
+++ lib/rea_agreement/zome_idx_planning/src/lib.rs
@@ -6,62 +6,12 @@
  *
  * @package Holo-REA
  */
-extern crate serde;
-extern crate hdk;
-extern crate hdk_proc_macros;
-
 use hdk::prelude::*;
-use hdk_proc_macros::zome;
 
 use hc_zome_rea_commitment_storage_consts::{ COMMITMENT_BASE_ENTRY_TYPE, COMMITMENT_CLAUSE_OF_LINK_TYPE };
 use hc_zome_rea_agreement_storage_consts::{ AGREEMENT_BASE_ENTRY_TYPE, AGREEMENT_COMMITMENTS_LINK_TYPE };
 
-#[zome]
-mod rea_agreement_index_zome {
-    #[init]
-    fn init() {
-        Ok(())
-    }
+entry_defs![ Path::entry_def() ];
 
-    #[validate_agent]
-    pub fn validate_agent(validation_data: EntryValidationData::<AgentId>) {
-        Ok(())
-    }
-
-    #[entry_def]
-    fn index_base_entry_def() -> ValidatingEntryType {
-        entry!(
-            name: AGREEMENT_BASE_ENTRY_TYPE,
-            description: "Base anchor for external Agreement records to provide lookup functionality",
-            sharing: Sharing::Public,
-            validation_package: || {
-                hdk::ValidationPackageDefinition::Entry
-            },
-            validation: |_validation_data: hdk::EntryValidationData<Address>| {
-                Ok(())
-            },
-            links: [
-                to!(
-                    COMMITMENT_BASE_ENTRY_TYPE,
-                    link_type: AGREEMENT_COMMITMENTS_LINK_TYPE,
-                    validation_package: || {
-                        hdk::ValidationPackageDefinition::Entry
-                    },
-                    validation: | _validation_data: hdk::LinkValidationData| {
-                        Ok(())
-                    }
-                ),
-                from!(
-                    COMMITMENT_BASE_ENTRY_TYPE,
-                    link_type: COMMITMENT_CLAUSE_OF_LINK_TYPE,
-                    validation_package: || {
-                        hdk::ValidationPackageDefinition::Entry
-                    },
-                    validation: | _validation_data: hdk::LinkValidationData| {
-                        Ok(())
-                    }
-                )
-            ]
-        )
-    }
-}
+// :TODO: decide if these types of indexing zomes are necessary for exposing any API endpoints
+// @see ../zome_idx_observation for more
