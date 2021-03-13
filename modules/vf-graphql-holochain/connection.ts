e375a9b4cd870f9462f61d602ae680a50a095306
pospi <pospi@spadgos.com>
2021-03-13 19:00:17 +1000

    use client connection awareness of CellId to handle encoding of identifiers at API boundary

--- modules/vf-graphql-holochain/connection.ts
+++ modules/vf-graphql-holochain/connection.ts
@@ -1,10 +1,7 @@
 /**
  * Connection wrapper for Holochain DNA method calls
  *
- * :TODO: provide a facility for this to be wired into Redux, allow
- * multiple connection sources & generally be more modular
- *
- * @package: HoloREA
+ * @package: Holo-REA
  * @since:   2019-05-20
  */
 
@@ -13,6 +10,11 @@ import deepForEach from 'deep-for-each'
 import { format, parse } from 'fecha'
 import { Base64 } from "js-base64"
 import { DNAIdMappings } from './types'
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// Connection persistence and multi-conductor / multi-agent handling
+//----------------------------------------------------------------------------------------------------------------------
 
 let DEFAULT_CONNECTION_URI = process.env.REACT_APP_HC_CONN_URL || ''
 const CONNECTION_CACHE: { [i: string]: Promise<AppWebsocket> } = {}
@@ -53,34 +55,49 @@ const getConnection = (socketURI: string) => {
   return CONNECTION_CACHE[socketURI]
 }
 
+
+//----------------------------------------------------------------------------------------------------------------------
+// Holochain / GraphQL type translation layer
+//----------------------------------------------------------------------------------------------------------------------
+
+// @see https://github.com/holochain-open-dev/core-types/blob/main/src/utils.ts
+function deserializeHash(hash: string): Uint8Array {
+  return Base64.toUint8Array(hash.slice(1));
+}
+
+// @see https://github.com/holochain-open-dev/core-types/blob/main/src/utils.ts
+function serializeHash(hash: Uint8Array): string {
+  return `u${Base64.fromUint8Array(hash, true)}`;
+}
+
 /**
  * Decode raw data input coming from Holochain API websocket.
  *
  * Mutates in place- we have no need for the non-normalised primitive format and this saves memory.
  */
-const decodeFields = (result: any): void => {
-  decodeIdFields(result)
+const decodeFields = (cellDNAHash: Buffer, result: any): void => {
+  coerceIdFields(cellDNAHash, result)
   decodeDateFields(result)
 }
 
 // check toplevel result objects for ID field buffers and coerce to strings for GraphQL
-const decodeIdFields = (result: any): void => {
+const coerceIdFields = (cellDNAHash: Buffer, result: any): void => {
+  let cellIdStr = serializeHash(cellDNAHash)
   let r
   for (let field of Object.keys(result)) {
     r = result[field]
-    if (r.cellId) {
-      if (r.id) {
-        r.id = `${r.cellId.toString('base64')}/${r.id.toString('base64')}`
-      }
-      if (r.revisionId) {
-        r.revisionId = `${r.cellId.toString('base64')}/${r.revisionId.toString('base64')}`
-      }
+    if (r.id) {
+      r.id = `${cellIdStr}/${serializeHash(r.id)}`
+    }
+    if (r.revisionId) {
+      r.revisionId = `${cellIdStr}/${serializeHash(r.revisionId)}`
     }
   }
 }
 
 const isoDateRegex = /^\d{4}-\d\d-\d\d(T\d\d:\d\d:\d\d(\.\d\d\d)?)?([+-]\d\d:\d\d)?$/
 
+// recursively check for Date strings and convert to JS date objects upon receiving
 const decodeDateFields = (result: any): void => {
   deepForEach(result, (value, prop, subject, path) => {
     if (value.match(isoDateRegex)) {
@@ -94,29 +111,31 @@ const decodeDateFields = (result: any): void => {
  *
  * Clones data in order to keep input data pristine.
  */
-const encodeFields = (args: any): any => {
-  args = encodeIdFields(args)
+const encodeFields = (cellDNAHash: Buffer, args: any): any => {
+  args = encodeIdFields(cellDNAHash, args)
   args = encodeDateFields(args)
   return args
 }
 
-// check toplevel result objects for ID fields and coerce to strings for GraphQL
-const encodeIdFields = (args: any): any => {
+// decode string versions of ID fields into Buffer objects for Holochain
+const encodeIdFields = (cellDNAHash: Buffer, args: any): any => {
   const res = {}
   let r
-  // pull toplevel parameter ID info out of string values into buffers before sending
+  // pull toplevel parameter ID info out of string values into simplified buffers before sending
   for (let field of Object.keys(args)) {
     r = args[field]
     if (r.revisionId) {
       const [cellId, revisionId] = parseSingleIdField(r.revisionId)
-      // :WARNING: presuming correctness and association here
-      r.cellId = cellId
+      if (cellId !== cellDNAHash) {
+        throw new Error(`Record data from ${r.revisionId} passed to incorrect cell ${cellDNAHash}`)
+      }
       r.revisionId = revisionId
     }
     if (r.id) {
       const [cellId, id] = parseSingleIdField(r.id)
-      // :WARNING: presuming correctness and association here
-      r.cellId = cellId
+      if (cellId !== cellDNAHash) {
+        throw new Error(`Record data from ${r.id} passed to incorrect cell ${cellDNAHash}`)
+      }
       r.id = id
     }
     res[field] = r
@@ -135,15 +154,18 @@ const encodeDateFields = (args: any): any => {
   return res
 }
 
-type BufferArr = [Buffer, Buffer]
-const parseSingleIdField = (field: string): BufferArr => {
-  const matches = field.match(/^(\w+)\/(\w+)$/)
-  if (!matches) throw new Error('Format error in ID field value')
+const parseSingleIdField = (field: string): CellId => {
+  const matches = field.split('/')
   return [
-    Buffer.from(matches[1], 'base64'),
-    Buffer.from(matches[2], 'base64'),
+    Buffer.from(deserializeHash(matches[1])),
+    Buffer.from(deserializeHash(matches[2])),
   ]
 }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// Holochain cell API method binding API
+//----------------------------------------------------------------------------------------------------------------------
 
 // explicit type-loss at the boundary
 export type BoundZomeFn = (args: any) => any;
@@ -159,9 +181,9 @@ const zomeFunction = (socketURI: string, cell_id: CellId, zome_name: string, fn_
     zome_name,
     fn_name,
     provenance: cell_id[1],
-    payload: encodeFields(args),
+    payload: encodeFields(cell_id[0], args),
   })
-  decodeFields(res)
+  decodeFields(cell_id[0], res)
   return res
 }
 
