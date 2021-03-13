f1a906632dc063c3285542696477970bfdd6eae0
pospi <pospi@spadgos.com>
2021-03-13 13:47:11 +1000

    remove old code to finish d0468e9

--- lib/hdk_type_serialization_macros/src/lib.rs
+++ lib/hdk_type_serialization_macros/src/lib.rs
@@ -5,11 +5,6 @@
  * To convert a plain `EntryHash` to its wrapped form, use `raw_address.into()`.
  */
 use holochain_serialized_bytes::prelude::*;
-use holo_hash::{ AgentPubKey, EntryHash, HeaderHash };
-
-// re-exports for convenience
-pub use chrono::{ Local, Utc, DateTime };
-pub use holochain_zome_types::timestamp::Timestamp;
 
 // A string wrapper around binary IDs which serializes to a string
 #[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone, PartialEq)]
@@ -74,36 +69,3 @@ macro_rules! hashed_identifier {
         }
     }
 }
-
-hashed_identifier!(RevisionHash => HeaderHash);
-
-simple_alias!(ActionId => String);
-
-simple_alias!(ExternalURL => String);
-
-hashed_identifier!(LocationAddress => EntryHash);
-
-simple_alias!(UnitId => String);
-
-hashed_identifier!(AgentAddress => AgentPubKey);
-
-hashed_identifier!(EventAddress => EntryHash);
-hashed_identifier!(ResourceAddress => EntryHash);
-hashed_identifier!(ProductBatchAddress => EntryHash);
-hashed_identifier!(ProcessAddress => EntryHash);
-
-hashed_identifier!(CommitmentAddress => EntryHash);
-hashed_identifier!(FulfillmentAddress => EntryHash);
-hashed_identifier!(IntentAddress => EntryHash);
-hashed_identifier!(SatisfactionAddress => EntryHash);
-hashed_identifier!(EventOrCommitmentAddress => EntryHash);
-
-hashed_identifier!(PlanAddress => EntryHash);
-hashed_identifier!(AgreementAddress => EntryHash);
-
-hashed_identifier!(ResourceSpecificationAddress => EntryHash);
-hashed_identifier!(ProcessSpecificationAddress => EntryHash);
-
-hashed_identifier!(ProposedIntentAddress => EntryHash);
-hashed_identifier!(ProposalAddress => EntryHash);
-hashed_identifier!(ProposedToAddress => EntryHash);
