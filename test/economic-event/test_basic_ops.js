6207f94e9012e5817fe9993b26d579af6820d0b9
pospi <pospi@spadgos.com>
2021-04-05 10:42:24 +1000

    update basic event test for RSM

--- test/economic-event/test_basic_ops.js
+++ test/economic-event/test_basic_ops.js
@@ -2,6 +2,7 @@ const {
   buildConfig,
   buildRunner,
   buildPlayer,
+  mockAgentId,
 } = require('../init')
 
 const runner = buildRunner()
@@ -14,19 +15,19 @@ runner.registerScenario('create simplest event', async (s, t) => {
   const event = {
     note: 'test event',
     action: 'raise',
-    provider: 'todo-some-agent-id',
-    receiver: 'todo-some-agent-id',
-    hasPointInTime: '2019-11-19T12:12:42.739Z',
+    provider: mockAgentId(),
+    receiver: mockAgentId(),
+    hasPointInTime: '2019-11-19T12:12:42.739+01:00',
     resourceClassifiedAs: ['some-resource-type'],
     resourceQuantity: { hasNumericalValue: 1 },
     inScopeOf: ['some-accounting-scope'],
   }
 
-  const createEventResponse = await alice.call('observation', 'economic_event', 'create_event', { event })
+  const createEventResponse = await alice.call('economic_event', 'create_event', { event })
   await s.consistency()
 
-  t.ok(createEventResponse.Ok.economicEvent, 'event created')
-  t.deepEqual(createEventResponse.Ok.economicEvent.inScopeOf, ['some-accounting-scope'], 'event inScopeOf saved')
+  t.ok(createEventResponse.economicEvent, 'event created')
+  t.deepEqual(createEventResponse.economicEvent.inScopeOf, ['some-accounting-scope'], 'event inScopeOf saved')
 })
 
 runner.run()
