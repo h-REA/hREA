fd5b8f6e3f0627ab01435dca7bd5fa57d2a98da8
pospi <pospi@spadgos.com>
2021-08-22 17:02:40 +1000

    first pass at Fulfillment & Satisfaction origin zomes. Fixes some ommissions regarding index updates.

--- zomes/rea_fulfillment/lib_origin/src/lib.rs
+++ zomes/rea_fulfillment/lib_origin/src/lib.rs
@@ -9,146 +9,182 @@
 
  * @package Holo-REA
  */
-use hdk::prelude::*;
-use hdk::{
-    PUBLIC_TOKEN,
-    call,
-};
-
 use hdk_records::{
+    RecordAPIResult, OtherCellResult, DataIntegrityError,
     records::{
         create_record,
         read_record_entry,
+        read_record_entry_by_header,
         update_record,
         delete_record,
     },
     local_indexes::{
-        create_direct_index,
-        query_direct_index_with_foreign_key,
+        create_index,
+        update_index,
+        query_index,
     },
+    rpc::call_zome_method,
 };
 
-use hc_zome_rea_commitment_storage_consts::{COMMITMENT_FULFILLEDBY_LINK_TYPE, COMMITMENT_FULFILLEDBY_LINK_TAG};
+use hc_zome_rea_commitment_storage_consts::{COMMITMENT_FULFILLEDBY_LINK_TAG};
 use hc_zome_rea_fulfillment_storage_consts::*;
-use hc_zome_rea_fulfillment_storage::Entry;
+use hc_zome_rea_fulfillment_storage::*;
 use hc_zome_rea_fulfillment_rpc::*;
 use hc_zome_rea_fulfillment_lib::construct_response;
 
-pub fn receive_create_fulfillment(fulfillment: CreateRequest) -> ZomeApiResult<ResponseData> {
-    handle_create_fulfillment(&fulfillment)
+pub fn receive_create_fulfillment<S>(entry_def_id: S, commitment_entry_def_id: S, fulfillment: CreateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    handle_create_fulfillment(entry_def_id, commitment_entry_def_id, &fulfillment)
 }
 
-pub fn receive_get_fulfillment(address: FulfillmentAddress) -> ZomeApiResult<ResponseData> {
-    handle_get_fulfillment(&address)
+pub fn receive_get_fulfillment<S>(entry_def_id: S, address: FulfillmentAddress) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    handle_get_fulfillment(entry_def_id, &address)
 }
 
-pub fn receive_update_fulfillment(fulfillment: UpdateRequest) -> ZomeApiResult<ResponseData> {
-    handle_update_fulfillment(&fulfillment)
+pub fn receive_update_fulfillment<S>(entry_def_id: S, commitment_entry_def_id: S, fulfillment: UpdateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    handle_update_fulfillment(entry_def_id, commitment_entry_def_id, &fulfillment)
 }
 
-pub fn receive_delete_fulfillment(address: FulfillmentAddress) -> ZomeApiResult<bool> {
-    handle_delete_fulfillment(&address)
+pub fn receive_delete_fulfillment<S>(entry_def_id: S, commitment_entry_def_id: S, address: RevisionHash) -> RecordAPIResult<bool>
+    where S: AsRef<str>
+{
+    handle_delete_fulfillment(entry_def_id, commitment_entry_def_id, &address)
 }
 
-pub fn receive_query_fulfillments(params: QueryParams) -> ZomeApiResult<Vec<ResponseData>> {
-    handle_query_fulfillments(&params)
+pub fn receive_query_fulfillments<S>(entry_def_id: S, params: QueryParams) -> RecordAPIResult<Vec<ResponseData>>
+    where S: AsRef<str>
+{
+    handle_query_fulfillments(entry_def_id, &params)
 }
 
-fn handle_create_fulfillment(fulfillment: &CreateRequest) -> ZomeApiResult<ResponseData> {
-    let (fulfillment_address, entry_resp): (FulfillmentAddress, Entry) = create_record(
-        FULFILLMENT_BASE_ENTRY_TYPE, FULFILLMENT_ENTRY_TYPE,
-        FULFILLMENT_INITIAL_ENTRY_LINK_TYPE,
-        fulfillment.to_owned(),
-    )?;
+fn handle_create_fulfillment<S>(entry_def_id: S, commitment_entry_def_id: S, fulfillment: &CreateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    let (revision_id, fulfillment_address, entry_resp): (_,_, EntryData) = create_record(&entry_def_id, fulfillment.to_owned())?;
 
     // link entries in the local DNA
-    let _results = create_direct_index(
-        fulfillment_address.as_ref(),
-        fulfillment.get_fulfills().as_ref(),
-        FULFILLMENT_FULFILLS_LINK_TYPE, FULFILLMENT_FULFILLS_LINK_TAG,
-        COMMITMENT_FULFILLEDBY_LINK_TYPE, COMMITMENT_FULFILLEDBY_LINK_TAG,
-    );
+    let _results = create_index(
+        &entry_def_id, &fulfillment_address,
+        &commitment_entry_def_id, fulfillment.get_fulfills(),
+        FULFILLMENT_FULFILLS_LINK_TAG, COMMITMENT_FULFILLEDBY_LINK_TAG,
+    )?;
 
     // update in the associated foreign DNA as well
-    let _pingback = call(
-        BRIDGED_OBSERVATION_DHT,
-        "fulfillment",
-        Address::from(PUBLIC_TOKEN.to_string()),
-        "fulfillment_created",
-        FwdCreateRequest { fulfillment: fulfillment.to_owned() }.into()
+    let _pingback: OtherCellResult<ResponseData> = call_zome_method(
+        fulfillment.get_fulfills(),
+        &REPLICATE_CREATE_API_METHOD,
+        CreateParams { fulfillment: fulfillment.to_owned() },
     );
+    // :TODO: report any error
 
-    Ok(construct_response(&fulfillment_address, &entry_resp))
+    construct_response(&fulfillment_address, &revision_id, &entry_resp)
 }
 
 /// Read an individual fulfillment's details
-fn handle_get_fulfillment(base_address: &FulfillmentAddress) -> ZomeApiResult<ResponseData> {
-    let entry = read_record_entry(base_address)?;
-    Ok(construct_response(&base_address, &entry))
+fn handle_get_fulfillment<S>(entry_def_id: S, address: &FulfillmentAddress) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    let (revision, base_address, entry) = read_record_entry::<EntryData, EntryStorage, _,_>(&entry_def_id, address.as_ref())?;
+    construct_response(&base_address, &revision, &entry)
 }
 
-fn handle_update_fulfillment(fulfillment: &UpdateRequest) -> ZomeApiResult<ResponseData> {
-    let base_address = fulfillment.get_id();
-    let new_entry = update_record(FULFILLMENT_ENTRY_TYPE, &base_address, fulfillment)?;
+fn handle_update_fulfillment<S>(entry_def_id: S, commitment_entry_def_id: S, fulfillment: &UpdateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    let (revision_id, base_address, new_entry, prev_entry): (_, FulfillmentAddress, EntryData, EntryData) = update_record(&entry_def_id, &fulfillment.get_revision_id(), fulfillment.to_owned())?;
 
-    // update in the associated foreign DNA as well
-    let _pingback = call(
-        BRIDGED_OBSERVATION_DHT,
-        "fulfillment",
-        Address::from(PUBLIC_TOKEN.to_string()),
-        "fulfillment_updated",
-        FwdUpdateRequest { fulfillment: fulfillment.clone() }.into()
+    // update commitment indexes in local DNA
+    if new_entry.fulfills != prev_entry.fulfills {
+        let _results = update_index(
+            &entry_def_id, &base_address,
+            &commitment_entry_def_id,
+            FULFILLMENT_FULFILLS_LINK_TAG, COMMITMENT_FULFILLEDBY_LINK_TAG,
+            vec![new_entry.fulfills.clone()].as_slice(), vec![prev_entry.fulfills].as_slice(),
+        )?;
+    }
+
+    // update fulfillment records in remote DNA (and by proxy, event indexes in remote DNA)
+    if new_entry.fulfilled_by != prev_entry.fulfilled_by {
+        let _pingback: OtherCellResult<ResponseData> = call_zome_method(
+            // :TODO: update to intelligently call remote DNAs if new & old target record are not in same network
+            &prev_entry.fulfilled_by,
+            &REPLICATE_UPDATE_API_METHOD,
+            UpdateParams { fulfillment: fulfillment.to_owned() },
+        );
+        // :TODO: report any error
+    }
+
+    construct_response(&base_address, &revision_id, &new_entry)
+}
+
+fn handle_delete_fulfillment<S>(entry_def_id: S, commitment_entry_def_id: S, revision_id: &RevisionHash) -> RecordAPIResult<bool>
+    where S: AsRef<str>
+{
+    let (base_address, entry) = read_record_entry_by_header::<EntryData, EntryStorage, _>(&revision_id)?;
+
+    // update commitment indexes in local DNA
+    let _results = update_index(
+        &entry_def_id, &base_address,
+        &commitment_entry_def_id,
+        FULFILLMENT_FULFILLS_LINK_TAG, COMMITMENT_FULFILLEDBY_LINK_TAG,
+        vec![].as_slice(), vec![entry.fulfills].as_slice(),
+    )?;
+
+    // update fulfillment records in remote DNA (and by proxy, event indexes in remote DNA)
+    let _pingback: OtherCellResult<ResponseData> = call_zome_method(
+        &entry.fulfilled_by,
+        &REPLICATE_DELETE_API_METHOD,
+        ByHeader { address: revision_id.to_owned() },
     );
+    // :TODO: report any error
 
-    Ok(construct_response(base_address, &new_entry))
+    delete_record::<EntryStorage, _>(&revision_id)
 }
 
-fn handle_delete_fulfillment(address: &FulfillmentAddress) -> ZomeApiResult<bool> {
-    let result = delete_record::<Entry>(address);
-
-    // update in the associated foreign DNA as well
-    let _pingback = call(
-        BRIDGED_OBSERVATION_DHT,
-        "fulfillment",
-        Address::from(PUBLIC_TOKEN.to_string()),
-        "fulfillment_deleted",
-        address.into(),
-    );
-
-    result
-}
-
-fn handle_query_fulfillments(params: &QueryParams) -> ZomeApiResult<Vec<ResponseData>> {
-    let mut entries_result: ZomeApiResult<Vec<(FulfillmentAddress, Option<Entry>)>> = Err(ZomeApiError::Internal("No results found".to_string()));
+fn handle_query_fulfillments<S>(commitment_entry_def_id: S, params: &QueryParams) -> RecordAPIResult<Vec<ResponseData>>
+    where S: AsRef<str>
+{
+    let mut entries_result: RecordAPIResult<Vec<RecordAPIResult<(RevisionHash, FulfillmentAddress, EntryData)>>> = Err(DataIntegrityError::EmptyQuery);
 
     // :TODO: proper search logic, not mutually exclusive ID filters
     match &params.fulfills {
         Some(fulfills) => {
-            entries_result = query_direct_index_with_foreign_key(fulfills, COMMITMENT_FULFILLEDBY_LINK_TYPE, COMMITMENT_FULFILLEDBY_LINK_TAG);
+            entries_result = query_index::<EntryData, EntryStorage, _,_,_,_>(
+                &commitment_entry_def_id,
+                fulfills, COMMITMENT_FULFILLEDBY_LINK_TAG,
+            );
         },
         _ => (),
     };
-    // :TODO: observation DNA handles this. Should queries be possible in planning DNA, too?
-    // match &params.fulfilled_by {
-    //     Some(fulfilled_by) => {
-    //         entries_result = query_direct_index_with_foreign_key(fulfilled_by, EVENT_FULFILLS_LINK_TYPE, EVENT_FULFILLS_LINK_TAG);
-    //     },
-    //     _ => (),
-    // };
 
     match entries_result {
-        Ok(entries) => Ok(
-            entries.iter()
-                .map(|(entry_base_address, maybe_entry)| {
-                    // :TODO: avoid cloning entry
-                    match maybe_entry {
-                        Some(entry) => Ok(construct_response(entry_base_address, entry)),
-                        None => Err(ZomeApiError::Internal("referenced entry not found".to_string()))
-                    }
-                })
+        Err(DataIntegrityError::EmptyQuery) => Ok(vec![]),
+        Err(e) => Err(e),
+        _ => {
+            Ok(handle_list_output(entries_result?)?.iter().cloned()
                 .filter_map(Result::ok)
                 .collect()
-        ),
-        _ => Err(ZomeApiError::Internal("could not load linked addresses".to_string()))
+            )
+        },
     }
+}
+
+// :DUPE: query-list-output-no-links
+fn handle_list_output(entries_result: Vec<RecordAPIResult<(RevisionHash, FulfillmentAddress, EntryData)>>) -> RecordAPIResult<Vec<RecordAPIResult<ResponseData>>>
+{
+    Ok(entries_result.iter()
+        .cloned()
+        .filter_map(Result::ok)
+        .map(|(revision_id, entry_base_address, entry)| {
+            construct_response(
+                &entry_base_address, &revision_id, &entry,
+            )
+        })
+        .collect()
+    )
 }
--- zomes/rea_satisfaction/lib_origin/src/lib.rs
+++ zomes/rea_satisfaction/lib_origin/src/lib.rs
@@ -9,178 +9,251 @@
  *
  * @package Holo-REA
  */
-use hdk::{
-    PUBLIC_TOKEN,
-    THIS_INSTANCE,
-    holochain_persistence_api::cas::content::Address,
-    error::{ZomeApiResult, ZomeApiError},
-    call,
-};
-
 use hdk_records::{
+    RecordAPIResult, OtherCellResult, DataIntegrityError,
     records::{
         create_record,
         read_record_entry,
+        read_record_entry_by_header,
         update_record,
         delete_record,
     },
-    rpc::read_from_zome,
+    rpc::{
+        call_zome_method,
+        call_local_zome_method,
+    },
     local_indexes::{
-        query_direct_index_with_foreign_key,
-        create_direct_index,
+        create_index,
+        update_index,
+        query_index,
     },
 };
 
-use hc_zome_rea_intent_storage_consts::{INTENT_SATISFIEDBY_LINK_TYPE, INTENT_SATISFIEDBY_LINK_TAG};
-use hc_zome_rea_commitment_storage_consts::{COMMITMENT_SATISFIES_LINK_TYPE, COMMITMENT_SATISFIES_LINK_TAG};
+use hc_zome_rea_intent_storage_consts::{INTENT_SATISFIEDBY_LINK_TAG};
+use hc_zome_rea_commitment_storage_consts::{COMMITMENT_SATISFIES_LINK_TAG};
 use hc_zome_rea_commitment_rpc::{ResponseData as CommitmentResponse};
 use hc_zome_rea_satisfaction_storage_consts::*;
 use hc_zome_rea_satisfaction_storage::*;
 use hc_zome_rea_satisfaction_rpc::*;
 use hc_zome_rea_satisfaction_lib::construct_response;
 
-pub fn receive_create_satisfaction(satisfaction: CreateRequest) -> ZomeApiResult<ResponseData> {
-    handle_create_satisfaction(&satisfaction)
+pub fn receive_create_satisfaction<S>(entry_def_id: S, intent_entry_def_id: S, commitment_entry_def_id: S, satisfaction: CreateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    handle_create_satisfaction(entry_def_id, intent_entry_def_id, commitment_entry_def_id, &satisfaction)
 }
 
-pub fn receive_get_satisfaction(address: SatisfactionAddress) -> ZomeApiResult<ResponseData> {
-    handle_get_satisfaction(&address)
+pub fn receive_get_satisfaction<S>(entry_def_id: S, address: SatisfactionAddress) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    handle_get_satisfaction(entry_def_id, &address)
 }
 
-pub fn receive_update_satisfaction(satisfaction: UpdateRequest) -> ZomeApiResult<ResponseData> {
-    handle_update_satisfaction(&satisfaction)
+pub fn receive_update_satisfaction<S>(entry_def_id: S, intent_entry_def_id: S, commitment_entry_def_id: S, satisfaction: UpdateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    handle_update_satisfaction(entry_def_id, intent_entry_def_id, commitment_entry_def_id, &satisfaction)
 }
 
-pub fn receive_delete_satisfaction(address: SatisfactionAddress) -> ZomeApiResult<bool> {
-    handle_delete_satisfaction(&address)
+pub fn receive_delete_satisfaction<S>(entry_def_id: S, intent_entry_def_id: S, commitment_entry_def_id: S, revision_id: RevisionHash) -> RecordAPIResult<bool>
+    where S: AsRef<str>
+{
+    handle_delete_satisfaction(entry_def_id, intent_entry_def_id, commitment_entry_def_id, &revision_id)
 }
 
-pub fn receive_query_satisfactions(params: QueryParams) -> ZomeApiResult<Vec<ResponseData>> {
-    handle_query_satisfactions(&params)
+pub fn receive_query_satisfactions<S>(intent_entry_def_id: S, commitment_entry_def_id: S, params: QueryParams) -> RecordAPIResult<Vec<ResponseData>>
+    where S: AsRef<str>
+{
+    handle_query_satisfactions(intent_entry_def_id, commitment_entry_def_id, &params)
 }
 
-fn handle_create_satisfaction(satisfaction: &CreateRequest) -> ZomeApiResult<ResponseData> {
-    let (satisfaction_address, entry_resp): (SatisfactionAddress, Entry) = create_record(
-        SATISFACTION_BASE_ENTRY_TYPE, SATISFACTION_ENTRY_TYPE,
-        SATISFACTION_INITIAL_ENTRY_LINK_TYPE,
-        satisfaction.to_owned(),
-    )?;
+fn is_satisfiedby_commitment(event_or_commitment: &EventOrCommitmentAddress) -> OtherCellResult<CommitmentResponse> {
+    call_local_zome_method(
+        |conf: DnaConfigSlice| { conf.satisfaction.commitment_zome },
+        &CHECK_COMMITMENT_API_METHOD,
+        CheckCommitmentRequest { address: event_or_commitment.to_owned().into() },
+    )
+}
+
+fn handle_create_satisfaction<S>(entry_def_id: S, intent_entry_def_id: S, commitment_entry_def_id: S, satisfaction: &CreateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    let (revision_id, satisfaction_address, entry_resp): (_,_, EntryData) = create_record(&entry_def_id, satisfaction.to_owned())?;
 
     // link entries in the local DNA
-    let _results1 = create_direct_index(
-        satisfaction_address.as_ref(),
-        satisfaction.get_satisfies().as_ref(),
-        SATISFACTION_SATISFIES_LINK_TYPE, SATISFACTION_SATISFIES_LINK_TAG,
-        INTENT_SATISFIEDBY_LINK_TYPE, INTENT_SATISFIEDBY_LINK_TAG,
-    );
+    let _results1 = create_index(
+        &entry_def_id, &satisfaction_address,
+        &intent_entry_def_id, satisfaction.get_satisfies(),
+        SATISFACTION_SATISFIES_LINK_TAG, INTENT_SATISFIEDBY_LINK_TAG,
+    )?;
 
     // link entries which may be local or remote
     // :TODO: Should not have to do this-
     //        One option is that linking to a nonexistent entry should autocreate the base.
     //        This would also make it safe to create things out of order at the expense of validation of external data.
     //        (Alternative: every link has to get a successful pingback from the destination object with its trait signature intact.)
+    //        Could also probably just run both ops and return OK if one succeeds; overhead would be similar to checking.
     // :TODO: use of URIs and a Holochain protocol resolver would also make this type of logic entirely unnecessary
+    // :TODO: actually this will probably fail under the new model since may not have updated values if accessing via WASM
     let event_or_commitment = satisfaction.get_satisfied_by();
-    let satisfying_commitment: ZomeApiResult<CommitmentResponse> = read_from_zome(
-        THIS_INSTANCE,
-        "commitment",
-        Address::from(PUBLIC_TOKEN.to_string()),    // :TODO:
-        "get_commitment",
-        CheckCommitmentRequest { address: event_or_commitment.to_owned().into() }.into(),
-    );
+    let satisfying_commitment = is_satisfiedby_commitment(event_or_commitment);
 
     match satisfying_commitment {
         // links to local commitment, create link index pair
         Ok(_) => {
-            let _results2 = create_direct_index(
-                satisfaction_address.as_ref(),
-                event_or_commitment.as_ref().into(),
-                SATISFACTION_SATISFIEDBY_LINK_TYPE, SATISFACTION_SATISFIEDBY_LINK_TAG,
-                COMMITMENT_SATISFIES_LINK_TYPE, COMMITMENT_SATISFIES_LINK_TAG,
-            );
+            let _results2 = create_index(
+                &entry_def_id, &satisfaction_address,
+                &commitment_entry_def_id, event_or_commitment,
+                SATISFACTION_SATISFIEDBY_LINK_TAG, COMMITMENT_SATISFIES_LINK_TAG,
+            )?;
         },
         // links to remote event, ping associated foreign DNA
         _ => {
-            let _pingback = call(
-                BRIDGED_OBSERVATION_DHT,
-                "satisfaction",
-                Address::from(PUBLIC_TOKEN.to_string()),    // :TODO:
-                "satisfaction_created",
-                FwdCreateRequest { satisfaction: satisfaction.to_owned() }.into()
+            let _pingback: OtherCellResult<ResponseData> = call_zome_method(
+                event_or_commitment,
+                &REPLICATE_CREATE_API_METHOD,
+                FwdCreateRequest { satisfaction: satisfaction.to_owned() }
             );
         },
     };
 
-    Ok(construct_response(&satisfaction_address, &entry_resp))
+    construct_response(&satisfaction_address, &revision_id, &entry_resp)
 }
 
 /// Read an individual satisfaction's details
-fn handle_get_satisfaction(base_address: &SatisfactionAddress) -> ZomeApiResult<ResponseData> {
-    let entry = read_record_entry(base_address)?;
-    Ok(construct_response(&base_address, &entry))
+fn handle_get_satisfaction<S>(entry_def_id: S, address: &SatisfactionAddress) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    let (revision, base_address, entry) = read_record_entry::<EntryData, EntryStorage, _,_>(&entry_def_id, address.as_ref())?;
+    construct_response(&base_address, &revision, &entry)
 }
 
-fn handle_update_satisfaction(satisfaction: &UpdateRequest) -> ZomeApiResult<ResponseData> {
-    let base_address = satisfaction.get_id();
-    let new_entry = update_record(SATISFACTION_ENTRY_TYPE, &base_address, satisfaction)?;
+fn handle_update_satisfaction<S>(entry_def_id: S, intent_entry_def_id: S, commitment_entry_def_id: S, satisfaction: &UpdateRequest) -> RecordAPIResult<ResponseData>
+    where S: AsRef<str>
+{
+    let (revision_id, base_address, new_entry, prev_entry): (_, SatisfactionAddress, EntryData, EntryData) = update_record(&entry_def_id, &satisfaction.get_revision_id(), satisfaction.to_owned())?;
 
-    // update in the associated foreign DNA as well
-    let _pingback = call(
-        BRIDGED_OBSERVATION_DHT,
-        "satisfaction",
-        Address::from(PUBLIC_TOKEN.to_string()),
-        "satisfaction_updated",
-        FwdUpdateRequest { satisfaction: satisfaction.clone() }.into()
-    );
+    // update intent indexes in local DNA
+    if new_entry.satisfies != prev_entry.satisfies {
+        let _results = update_index(
+            &entry_def_id, &base_address,
+            &intent_entry_def_id,
+            SATISFACTION_SATISFIES_LINK_TAG, INTENT_SATISFIEDBY_LINK_TAG,
+            vec![new_entry.satisfies.clone()].as_slice(), vec![prev_entry.satisfies].as_slice(),
+        )?;
+    }
 
-    Ok(construct_response(base_address, &new_entry))
+    // update commitment / event indexes in local and/or remote DNA
+    if new_entry.satisfied_by != prev_entry.satisfied_by {
+        let _results = update_index(
+            &entry_def_id, &base_address,
+            &commitment_entry_def_id,
+            SATISFACTION_SATISFIEDBY_LINK_TAG, COMMITMENT_SATISFIES_LINK_TAG,
+            vec![new_entry.satisfied_by.clone()].as_slice(), vec![prev_entry.satisfied_by.clone()].as_slice(),
+        )?;
+
+        // update satisfaction records in remote DNA (and by proxy, indexes held there)
+        let _pingback: OtherCellResult<ResponseData> = call_zome_method(
+            // :TODO: update to intelligently call remote DNAs if new & old target record are not in same network
+            &prev_entry.satisfied_by,
+            &REPLICATE_UPDATE_API_METHOD,
+            UpdateParams { satisfaction: satisfaction.clone() },
+        );
+
+        // :TODO: ensure exactly 1 operation succeeded
+    }
+
+    construct_response(&base_address, &revision_id, &new_entry)
 }
 
-fn handle_delete_satisfaction(address: &SatisfactionAddress) -> ZomeApiResult<bool> {
-    let result = delete_record::<Entry>(address);
+fn handle_delete_satisfaction<S>(entry_def_id: S, intent_entry_def_id: S, commitment_entry_def_id: S, revision_id: &RevisionHash) -> RecordAPIResult<bool>
+    where S: AsRef<str>
+{
+    let (base_address, entry) = read_record_entry_by_header::<EntryData, EntryStorage, _>(&revision_id)?;
 
-    // update in the associated foreign DNA as well
-    let _pingback = call(
-        BRIDGED_OBSERVATION_DHT,
-        "satisfaction",
-        Address::from(PUBLIC_TOKEN.to_string()),
-        "satisfaction_deleted",
-        address.into(),
-    );
+    // update intent indexes in local DNA
+    let _results = update_index(
+        &entry_def_id, &base_address,
+        &intent_entry_def_id,
+        SATISFACTION_SATISFIES_LINK_TAG, INTENT_SATISFIEDBY_LINK_TAG,
+        vec![].as_slice(), vec![entry.satisfies].as_slice(),
+    )?;
 
-    result
+    // :TODO: implement URI resolving logic so as to not have to make this check
+    let event_or_commitment = entry.satisfied_by.clone();
+    let satisfying_commitment = is_satisfiedby_commitment(&event_or_commitment);
+
+    match satisfying_commitment {
+        // links to local commitment, create link index pair
+        Ok(_) => {
+            let _results2 = update_index(
+                &entry_def_id, &base_address,
+                &commitment_entry_def_id,
+                SATISFACTION_SATISFIEDBY_LINK_TAG, COMMITMENT_SATISFIES_LINK_TAG,
+                vec![].as_slice(), vec![entry.satisfied_by].as_slice(),
+            )?;
+        },
+        // links to remote event, ping associated foreign DNA to replicate deletion there
+        _ => {
+            let _pingback: OtherCellResult<ResponseData> = call_zome_method(
+                &event_or_commitment,
+                &REPLICATE_DELETE_API_METHOD,
+                ByHeader { address: revision_id.to_owned() },
+            );
+        },
+    };
+
+    delete_record::<EntryStorage, _>(&revision_id)
 }
 
-fn handle_query_satisfactions(params: &QueryParams) -> ZomeApiResult<Vec<ResponseData>> {
-    let mut entries_result: ZomeApiResult<Vec<(SatisfactionAddress, Option<Entry>)>> = Err(ZomeApiError::Internal("No results found".to_string()));
+fn handle_query_satisfactions<S>(intent_entry_def_id: S, commitment_entry_def_id: S, params: &QueryParams) -> RecordAPIResult<Vec<ResponseData>>
+    where S: AsRef<str>,
+{
+    let mut entries_result: RecordAPIResult<Vec<RecordAPIResult<(RevisionHash, SatisfactionAddress, EntryData)>>> = Err(DataIntegrityError::EmptyQuery);
 
     // :TODO: implement proper AND search rather than exclusive operations
     match &params.satisfies {
         Some(satisfies) => {
-            entries_result = query_direct_index_with_foreign_key(satisfies, INTENT_SATISFIEDBY_LINK_TYPE, INTENT_SATISFIEDBY_LINK_TAG);
+            entries_result = query_index::<EntryData, EntryStorage, _,_,_,_>(
+                &intent_entry_def_id,
+                satisfies, INTENT_SATISFIEDBY_LINK_TAG,
+            );
         },
         _ => (),
     };
     match &params.satisfied_by {
         Some(satisfied_by) => {
-            entries_result = query_direct_index_with_foreign_key(satisfied_by, COMMITMENT_SATISFIES_LINK_TYPE, COMMITMENT_SATISFIES_LINK_TAG);
+            entries_result = query_index::<EntryData, EntryStorage, _,_,_,_>(
+                &commitment_entry_def_id,
+                satisfied_by, COMMITMENT_SATISFIES_LINK_TAG,
+            );
         },
         _ => (),
     };
 
     match entries_result {
-        Ok(entries) => Ok(
-            entries.iter()
-                .map(|(entry_base_address, maybe_entry)| {
-                    // :TODO: avoid cloning entry
-                    match maybe_entry {
-                        Some(entry) => Ok(construct_response(entry_base_address, entry)),
-                        None => Err(ZomeApiError::Internal("referenced entry not found".to_string()))
-                    }
-                })
+        Err(DataIntegrityError::EmptyQuery) => Ok(vec![]),
+        Err(e) => Err(e),
+        _ => {
+            Ok(handle_list_output(entries_result?)?.iter().cloned()
                 .filter_map(Result::ok)
                 .collect()
-        ),
-        _ => Err(ZomeApiError::Internal("could not load linked addresses".to_string()))
+            )
+        },
     }
+}
+
+// :DUPE: query-list-output-no-links
+fn handle_list_output(entries_result: Vec<RecordAPIResult<(RevisionHash, SatisfactionAddress, EntryData)>>) -> RecordAPIResult<Vec<RecordAPIResult<ResponseData>>>
+{
+    Ok(entries_result.iter()
+        .cloned()
+        .filter_map(Result::ok)
+        .map(|(revision_id, entry_base_address, entry)| {
+            construct_response(
+                &entry_base_address, &revision_id, &entry,
+            )
+        })
+        .collect()
+    )
 }
