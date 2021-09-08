f61f94f5df614ace49ab865ee88112527e35b16a
pospi <pospi@spadgos.com>
2021-09-08 18:22:50 +1000

    update Nix configuration to sync with HDK3-flavoured dev env correctly, adds missing wasm-opt tooling

--- default.nix
+++ default.nix
@@ -1,29 +1,35 @@
-let
-
- # point this to your local config.nix file for this project
- # example.config.nix shows and documents a lot of the options
- config = import ./config.nix;
-
- # START HOLONIX IMPORT BOILERPLATE
- holonix = import (
-  if ! config.holonix.use-github
-  then config.holonix.local.path
-  else fetchTarball {
-   url = "https://github.com/${config.holonix.github.owner}/${config.holonix.github.repo}/tarball/${config.holonix.github.ref}";
-   sha256 = config.holonix.github.sha256;
-  }
- ) { config = config; use-stable-rust = true; };
- # END HOLONIX IMPORT BOILERPLATE
-
-in
-with holonix.pkgs;
 {
- dev-shell = stdenv.mkDerivation (holonix.shell // {
-  name = "dev-shell";
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/62659a81d9f2d25740c2b2f424b90f50a60eb80c.tar.gz"; }
+}:
 
-  buildInputs = [ ]
-   ++ holonix.shell.buildInputs
-   ++ config.buildInputs
-  ;
- });
+let
+  holonix = import (holonixPath) {
+    include = {
+        # making this explicit even though it's the default
+        holochainBinaries = true;
+    };
+
+    holochainVersionId = "custom";
+
+    holochainVersion = {
+      rev = "d003eb7a45f1d7125c4701332202761721793d68"; # .104
+      sha256 = "0qxadszm2a7807w49kfbj7cx6lr31qryxcyd2inyv7q5j7qbanf2";
+      cargoSha256 = "129wicin99kmxb2qwhll8f4q78gviyp73hrkm6klpkql6810y3jy";
+      bins = {
+        holochain = "holochain";
+        hc = "hc";
+      };
+
+      lairKeystoreHashes = {
+        sha256 = "0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
+        cargoSha256 = "1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
+      };
+    };
+  };
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  buildInputs = with nixpkgs; [
+    binaryen
+  ];
 }
