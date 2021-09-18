e0beff17f3e36b76a0e1bd43bfeec16da8a3b969
pospi <pospi@spadgos.com>
2021-09-18 13:26:06 +1000

    first pass update Actions zome for RSM

--- zomes/rea_action/zome/src/lib.rs
+++ zomes/rea_action/zome/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(proc_macro_hygiene)]
 /**
  * ValueFlows Actions zome
  *
@@ -8,7 +7,6 @@
  * @since:   2019-12-23
  */
 use hdk::prelude::*;
-use hdk_proc_macros::zome;
 
 use vf_attributes_hdk::{
     ActionId,
@@ -19,33 +17,20 @@ use vf_actions::{
     get_all_builtin_actions,
 };
 
-#[zome]
-mod rea_specification_actions_zome {
+#[derive(Debug, Serialize, Deserialize)]
+struct ById {
+    id: ActionId,
+}
 
-    #[init]
-    fn init() {
-        Ok(())
+#[hdk_extern]
+fn get_action(ById { id }: ById) -> ExternResult<Action> {
+    match get_builtin_action(id.as_ref()) {
+        Some(action) => Ok(action),
+        None => Err(WasmError::Guest(format!("No action with ID '{}' available", id.as_ref()))),
     }
+}
 
-    #[validate_agent]
-    pub fn validate_agent(validation_data: EntryValidationData::<AgentId>) {
-        Ok(())
-    }
-
-    // receive: |from, payload| {
-    //   format!("Received: {} from {}", payload, from)
-    // }
-
-    #[zome_fn("hc_public")]
-    fn get_action(id: ActionId) -> ZomeApiResult<Action> {
-        match get_builtin_action(id.as_ref()) {
-            Some(action) => Ok(action),
-            None => Err(ZomeApiError::Internal(format!("No action with ID '{}' available", id.as_ref()))),
-        }
-    }
-
-    #[zome_fn("hc_public")]
-    fn get_all_actions() -> ZomeApiResult<Vec<Action>> {
-        Ok(get_all_builtin_actions())
-    }
+#[hdk_extern]
+fn get_all_actions(_: ()) -> ExternResult<Vec<Action>> {
+    Ok(get_all_builtin_actions())
 }
