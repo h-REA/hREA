e00e48d2d2dd67e94a4298dbcfe4d2cadc2263ff
pospi <pospi@spadgos.com>
2021-09-19 17:09:38 +1000

    implement custom serialization for Action enums per updated Serde version API

--- lib/vf_actions/src/lib.rs
+++ lib/vf_actions/src/lib.rs
@@ -4,14 +4,15 @@
  * @package Holo-REA
  * @since
  */
+use serde::{Deserializer, Serializer, de::Error};
+
 use hdk::prelude::*;
 use vf_attributes_hdk::{ ActionId, ProcessAddress, ResourceAddress };
 
 pub mod builtins;
 pub use builtins::{ get_builtin_action, get_all_builtin_actions };
 
-#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone, Copy, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(SerializedBytes, Debug, Clone, Copy, PartialEq)]
 pub enum ActionEffect {
     // for 'process' events
     NoEffect,
@@ -19,6 +20,36 @@ pub enum ActionEffect {
     Decrement,
     // for 'transfer' events
     DecrementIncrement,
+}
+
+impl Serialize for ActionEffect {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(match *self {
+            ActionEffect::NoEffect => "noEffect",
+            ActionEffect::Increment => "increment",
+            ActionEffect::Decrement => "decrement",
+            ActionEffect::DecrementIncrement => "decrementIncrement",
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for ActionEffect {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "noEffect" => ActionEffect::NoEffect,
+            "increment" => ActionEffect::Increment,
+            "decrement" => ActionEffect::Decrement,
+            "decrementIncrement" => ActionEffect::DecrementIncrement,
+            &_ => Err(
+                D::Error::custom(format!("Invalid value for ActionEffect: {}", s.as_str()))
+            )?,
+        })
+    }
 }
 
 // actual underlying operations applied to particular resources are a subset of higher-level ActionEffect
@@ -29,12 +60,39 @@ pub enum ActionInventoryEffect {
     Decrement,
 }
 
-#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone, Copy, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(SerializedBytes, Debug, Clone, Copy, PartialEq)]
 pub enum ProcessType {
     NotApplicable,
     Input,
     Output,
+}
+
+impl Serialize for ProcessType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(match *self {
+            ProcessType::NotApplicable => "notApplicable",
+            ProcessType::Input => "input",
+            ProcessType::Output => "output",
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for ProcessType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "notApplicable" => ProcessType::NotApplicable,
+            "input" => ProcessType::Input,
+            "output" => ProcessType::Output,
+            &_ => Err(
+                D::Error::custom(format!("Invalid value for ProcessType: {}", s.as_str()))
+            )?,
+        })
+    }
 }
 
 #[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone, PartialEq)]
