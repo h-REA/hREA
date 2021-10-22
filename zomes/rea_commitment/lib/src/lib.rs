2e50ca62a58c9c9a421d02b9d28e41902f5bbd95
pospi <pospi@spadgos.com>
2021-10-22 16:43:34 +1000

    update Commitment indexes for HDK3, implement correct index update logic for changed optional fields

--- zomes/rea_commitment/lib/src/lib.rs
+++ zomes/rea_commitment/lib/src/lib.rs
@@ -6,6 +6,7 @@
  *
  * @package Holo-REA
  */
+use paste::paste;
 use hdk_records::{
     RecordAPIResult, MaybeUndefined,
     records::{
@@ -16,19 +17,8 @@ use hdk_records::{
         delete_record,
     },
 };
-use hdk_semantic_indexes_client_lib::{
-    read_local_index,
-    create_remote_index,
-    update_remote_index,
-};
+use hdk_semantic_indexes_client_lib::*;
 
-use vf_attributes_hdk::{
-    AgentAddress,
-    FulfillmentAddress,
-    SatisfactionAddress,
-};
-
-use hc_zome_rea_commitment_storage_consts::*;
 use hc_zome_rea_commitment_storage::*;
 use hc_zome_rea_commitment_rpc::*;
 
@@ -42,31 +32,13 @@ pub fn handle_create_commitment<S>(entry_def_id: S, commitment: CreateRequest) -
 
     // handle link fields
     if let CreateRequest { input_of: MaybeUndefined::Some(input_of), .. } = &commitment {
-        let _results = create_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_INPUT_INDEXING_API_METHOD,
-            &base_address,
-            &PROCESS_INPUT_INDEXING_API_METHOD,
-            vec![input_of.clone()].as_slice(),
-        )?;
+        create_index!(Remote(commitment.input_of(input_of), process.committed_inputs(&base_address)))?;
     };
     if let CreateRequest { output_of: MaybeUndefined::Some(output_of), .. } = &commitment {
-        let _results = create_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_OUTPUT_INDEXING_API_METHOD,
-            &base_address,
-            &PROCESS_OUTPUT_INDEXING_API_METHOD,
-            vec![output_of.clone()].as_slice(),
-        )?;
+        create_index!(Remote(commitment.output_of(output_of), process.committed_outputs(&base_address)))?;
     };
     if let CreateRequest { clause_of: MaybeUndefined::Some(clause_of), .. } = &commitment {
-        let _results = create_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_CLAUSEOF_INDEXING_API_METHOD,
-            &base_address,
-            &AGREEMENT_CLAUSE_INDEXING_API_METHOD,
-            vec![clause_of.clone()].as_slice(),
-        )?;
+        create_index!(Remote(commitment.clause_of(clause_of), agreement.commitments(&base_address)))?;
     };
 
     // :TODO: pass results from link creation rather than re-reading
@@ -84,40 +56,38 @@ pub fn handle_update_commitment<S>(entry_def_id: S, commitment: UpdateRequest) -
     where S: AsRef<str>
 {
     let address = commitment.get_revision_id().to_owned();
-    let (revision_id, base_address, new_entry, _prev_entry): (_, CommitmentAddress, EntryData, EntryData) = update_record(&entry_def_id, &address, commitment.to_owned())?;
+    let (revision_id, base_address, new_entry, prev_entry): (_, CommitmentAddress, EntryData, EntryData) = update_record(&entry_def_id, &address, commitment.to_owned())?;
 
     // handle link fields
-    // :TODO: revise this logic; it creates dangling pointers. Need to check old record and ignore unchanged value, delete on removal.
-
-    if let UpdateRequest { input_of: MaybeUndefined::Some(input_of), .. } = &commitment {
-        let _results = update_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_INPUT_INDEXING_API_METHOD,
-            &base_address,
-            &PROCESS_INPUT_INDEXING_API_METHOD,
-            vec![input_of.to_owned()].as_slice(),
-            vec![].as_slice(),
-        )?;
+    if new_entry.input_of != prev_entry.input_of {
+        let new_value = match &new_entry.input_of { Some(val) => vec![val.to_owned()], None => vec![] };
+        let prev_value = match &prev_entry.input_of { Some(val) => vec![val.to_owned()], None => vec![] };
+        update_index!(Remote(
+            commitment
+                .input_of(new_value.as_slice())
+                .not(prev_value.as_slice()),
+            process.committed_inputs(&base_address)
+        ))?;
     }
-    if let UpdateRequest { output_of: MaybeUndefined::Some(output_of), .. } = &commitment {
-        let _results = update_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_OUTPUT_INDEXING_API_METHOD,
-            &base_address,
-            &PROCESS_OUTPUT_INDEXING_API_METHOD,
-            vec![output_of.to_owned()].as_slice(),
-            vec![].as_slice(),
-        );
+    if new_entry.output_of != prev_entry.output_of {
+        let new_value = match &new_entry.output_of { Some(val) => vec![val.to_owned()], None => vec![] };
+        let prev_value = match &prev_entry.output_of { Some(val) => vec![val.to_owned()], None => vec![] };
+        update_index!(Remote(
+            commitment
+                .output_of(new_value.as_slice())
+                .not(prev_value.as_slice()),
+            process.committed_outputs(&base_address)
+        ))?;
     }
-    if let UpdateRequest { clause_of: MaybeUndefined::Some(clause_of), .. } = commitment {
-        let _results = update_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_CLAUSEOF_INDEXING_API_METHOD,
-            &base_address,
-            &AGREEMENT_CLAUSE_INDEXING_API_METHOD,
-            vec![clause_of.to_owned()].as_slice(),
-            vec![].as_slice(),
-        );
+    if new_entry.clause_of != prev_entry.clause_of {
+        let new_value = match &new_entry.clause_of { Some(val) => vec![val.to_owned()], None => vec![] };
+        let prev_value = match &prev_entry.clause_of { Some(val) => vec![val.to_owned()], None => vec![] };
+        update_index!(Remote(
+            commitment
+                .clause_of(new_value.as_slice())
+                .not(prev_value.as_slice()),
+            agreement.commitments(&base_address)
+        ))?;
     }
 
     construct_response(&base_address, &revision_id, &new_entry, get_link_fields(&base_address)?)
@@ -130,34 +100,13 @@ pub fn handle_delete_commitment(revision_id: RevisionHash) -> RecordAPIResult<bo
 
     // handle link fields
     if let Some(process_address) = entry.input_of {
-        let _results = update_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_INPUT_INDEXING_API_METHOD,
-            &base_address,
-            &PROCESS_INPUT_INDEXING_API_METHOD,
-            vec![].as_slice(),
-            vec![process_address].as_slice(),
-        );
+        update_index!(Remote(commitment.input_of.not(vec![process_address].as_slice()), process.committed_inputs(&base_address)))?;
     }
     if let Some(process_address) = entry.output_of {
-        let _results = update_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_OUTPUT_INDEXING_API_METHOD,
-            &base_address,
-            &PROCESS_OUTPUT_INDEXING_API_METHOD,
-            vec![].as_slice(),
-            vec![process_address].as_slice(),
-        );
+        update_index!(Remote(commitment.output_of.not(vec![process_address].as_slice()), process.committed_outputs(&base_address)))?;
     }
     if let Some(agreement_address) = entry.clause_of {
-        let _results = update_remote_index(
-            read_foreign_index_zome,
-            &COMMITMENT_CLAUSEOF_INDEXING_API_METHOD,
-            &base_address,
-            &AGREEMENT_CLAUSE_INDEXING_API_METHOD,
-            vec![].as_slice(),
-            vec![agreement_address].as_slice(),
-        );
+        update_index!(Remote(commitment.clause_of.not(vec![agreement_address].as_slice()), agreement.commitments(&base_address)))?;
     }
 
     // delete entry last, as it must be present in order for links to be removed
@@ -212,7 +161,7 @@ fn construct_response<'a>(
 //---------------- READ ----------------
 
 /// Properties accessor for zome config
-fn read_foreign_index_zome(conf: DnaConfigSlice) -> Option<String> {
+fn read_commitment_index_zome(conf: DnaConfigSlice) -> Option<String> {
     Some(conf.commitment.index_zome)
 }
 
@@ -223,8 +172,8 @@ fn get_link_fields(commitment: &CommitmentAddress) -> RecordAPIResult<(
     Vec<AgentAddress>,
 )> {
     Ok((
-        read_local_index(read_foreign_index_zome, &COMMITMENT_FULFILLEDBY_READ_API_METHOD, commitment)?,
-        read_local_index(read_foreign_index_zome, &COMMITMENT_SATISFIES_READ_API_METHOD, commitment)?,
+        read_index!(commitment(commitment).fulfilled_by)?,
+        read_index!(commitment(commitment).satisfies)?,
         vec![],   // :TODO:
     ))
 }
