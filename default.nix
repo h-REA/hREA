42d9ba8525df13b4134b4ec181fae76e483c8d7a
pospi <pospi@spadgos.com>
2022-01-24 13:43:05 +1000

    update nix env

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
-  holonixRev = "ad7448859880cebe535187f54820ae2c9a3f4c08";
+  holonixRev = "2f7b8047d6314f64fca34394a52d465c18b2f4d5";
 
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_121";
+    holochainVersionId = "v0_0_123";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
