4c67931e8c5115512c585dd110bfde43d15cc27c
Connor Turland <connorturland@gmail.com>
2022-02-23 05:52:37 -0800

    add in original Path src code
    taken from https://github.com/holochain/holochain/blob/6d853bed86f59fa8a0a716d81c07e089b0d42f16/crates/hdk/src/hash_path.rs

---
+++ lib/temp_path/src/lib.rs
@@ -0,0 +1,7 @@
+/// TODO: this is a direct copy of what the code for Path USED
+/// to be, before the PathEntry change. This is here temporarily to keep
+/// the code in a working state, until a larger improvement on this system
+/// can be completed. This whole crate should be removed at that point.
+
+pub mod path;
+pub mod shard;
