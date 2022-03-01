1ebb329090bab09d177c948144a273b4ece0167f
Connor Turland <connorturland@gmail.com>
2022-03-01 10:15:16 -0800

    fixes #208 - update multiple times
    the original code didn't suit this context, and it was adapted from h-be/Acorn or hdk_crud.
    It didn't suit because in Acorn HeaderHash is used as "id" where as in holo-rea EntryHash is used as "id"
    hdk_crud creates a flat update tree, where all updates reference the original header
    holo-rea is supposed to create a branching Update tree, which this now allows for.
    
    However, this implementation is still very naive, and assumes that in fact the Update tree
    mostly just nests children in one sequence, more like an array than a tree. This won't hold
    true in practice, as we're dealing in offline-friendly distributed systems. Solving this properly
    relates to issue #196 conflict resolution

--- lib/hdk_records/src/record_helpers.rs
+++ lib/hdk_records/src/record_helpers.rs
@@ -45,7 +45,7 @@ pub fn get_latest_header_hash(entry_hash: EntryHash) -> RecordAPIResult<HeaderHa
         Some(Details::Entry(details)) => match details.entry_dht_status {
             metadata::EntryDhtStatus::Live => match details.updates.len() {
                 0 => {
-                    // no updates yet, latest header hash is the first one
+                    // https://docs.rs/hdk/latest/hdk/prelude/struct.EntryDetails.html#structfield.headers
                     Ok(get_header_hash(details.headers.first().unwrap().to_owned()))
                 },
                 _ => {
@@ -53,7 +53,8 @@ pub fn get_latest_header_hash(entry_hash: EntryHash) -> RecordAPIResult<HeaderHa
                     let mut sortlist = details.updates.to_vec();
                     sortlist.sort_by_key(|update| update.header().timestamp().as_micros());
                     let last = sortlist.last().unwrap().to_owned();
-                    Ok(get_header_hash(last))
+                    // recurse (unwrap should be safe because these are Update headers)
+                    get_latest_header_hash(last.header().entry_hash().unwrap().clone())
                 },
             },
             _ => Err(DataIntegrityError::EntryNotFound),
