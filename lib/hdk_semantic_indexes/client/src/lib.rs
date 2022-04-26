669903220670cb558edb1c1dfc9b660a02b9a701
pospi <pospi@spadgos.com>
2022-04-26 14:00:20 +1000

    fix empty config for locally-hosted index zome throwing an unnecessary error when remote indexes are being updated

--- lib/hdk_semantic_indexes/client/src/lib.rs
+++ lib/hdk_semantic_indexes/client/src/lib.rs
@@ -273,26 +273,44 @@ pub fn manage_index<C, F, G, A, B, S>(
 
     // Manage local index creation / removal
 
-    let local_forward_add = targets.local_dests.0.iter()
-        .map(|dest| {
-            request_sync_local_index(
-                origin_zome_name_from_config, origin_fn_name,
-                dest, &sources, &vec![],
-            )
-        });
-    let local_forward_remove = targets.local_dests.1.iter()
-        .map(|dest| {
-            request_sync_local_index(
-                origin_zome_name_from_config, origin_fn_name,
-                dest, &vec![], &sources,
-            )
-        });
-    let local_reciprocal_update = std::iter::once(request_sync_local_index(
-        dest_zome_name_from_config, dest_fn_name,
-        source, targets.local_dests.0.as_slice(), targets.local_dests.1.as_slice(),
-    ));
+    let empty = vec![];
+
+    let local_forward_add = (
+        if targets.local_dests.0.len() > 0 { targets.local_dests.0.iter() }
+        else { empty.iter() }
+    ).map(|dest| {
+        request_sync_local_index(
+            origin_zome_name_from_config, origin_fn_name,
+            dest, &sources, &vec![],
+        )
+    });
+
+    let local_forward_remove = (
+        if targets.local_dests.1.len() > 0 { targets.local_dests.1.iter() }
+        else { empty.iter() }
+    ).map(|dest| {
+        request_sync_local_index(
+            origin_zome_name_from_config, origin_fn_name,
+            dest, &vec![], &sources,
+        )
+    });
+
+    let mut local_updates = vec![];
+    let local_reciprocal_update =
+        if targets.local_dests.0.len() > 0 || targets.local_dests.1.len() > 0 {
+            let mut others = vec![request_sync_local_index(
+                dest_zome_name_from_config, dest_fn_name,
+                source, targets.local_dests.0.as_slice(), targets.local_dests.1.as_slice(),
+            )];
+            local_updates.append(&mut others);
+            local_updates.to_owned()
+        } else { vec![] };
 
     // Manage remote index creation / removal & append to resultset
+
+    // :TODO: improve error handling by asserting that successful RPC
+    // calls fired for local targets + remote targets add up to equal
+    // the number of input `dest_addresses` & `remove_addresses`
 
     Ok(std::iter::empty()
         .chain(local_forward_add)
