ca3fcacce7edc554c5a978143a4e0fd51da4a2dc
Connor Turland <connorturland@gmail.com>
2022-04-26 14:43:12 -0700

    fixes #248

--- test/core-architecture/test_record_links_cross_dna.js
+++ test/core-architecture/test_record_links_cross_dna.js
@@ -109,9 +109,9 @@ runner.registerScenario('updating remote link fields syncs fields and associated
 
   // SCENARIO: update link field (no-op)
   const ieResp3 = await planning.call('commitment', 'update_commitment', { commitment: updateCommitment })
-  const ieResp3RevisionId = ieResp3.revisionId
   t.deepEqual(ieResp3.commitment && ieResp3.commitment.inputOf, differentProcessId, 'update with same fields is no-op')
   await s.consistency()
+  const ieResp3RevisionId = ieResp3.commitment.revisionId
 
   // ASSERT: test event fields
   readResponse = await planning.call('commitment', 'get_commitment', { address: iCommitmentId })
@@ -153,77 +153,6 @@ runner.registerScenario('updating remote link fields syncs fields and associated
 
   // :TODO: attempt linking to nonexistent target (should this error, or happen regardless? Big question in distributed networks...)
   // :TODO: updates for fields when other values are present in the index array
-})
-
-runner.registerScenario('removing records with linked remote indexes clears them in associated records', async (s, t) => {
-  const { cells: [observation, planning] } = await buildPlayer(s, config, ['observation', 'planning'])
-
-  // SCENARIO: write initial records
-  const process = {
-    name: 'context record for testing relationships',
-  }
-  const pResp = await observation.call('process', 'create_process', { process })
-  t.ok(pResp.process && pResp.process.id, 'record created successfully')
-  await s.consistency()
-  const processId = pResp.process.id
-
-  const iIntent = {
-    note: 'test input intent',
-    inputOf: processId,
-    ...testEventProps,
-  }
-  const iiResp = await planning.call('intent', 'create_intent', { intent: iIntent })
-  t.ok(iiResp.intent && iiResp.intent.id, 'input record created successfully')
-  t.deepEqual(iiResp.intent.inputOf, processId, 'field reference OK in write')
-  await s.consistency()
-  const iIntentId = iiResp.intent.id
-  const iIntentRevisionId = iiResp.intent.revisionId
-
-  // ASSERT: test forward link field
-  let readResponse = await planning.call('intent', 'get_intent', { address: iIntentId })
-  t.deepEqual(readResponse.intent && readResponse.intent.inputOf, processId, 'field reference OK on read')
-
-  // ASSERT: test reciprocal link field
-  readResponse = await observation.call('process', 'get_process', { address: processId })
-  t.deepEqual(readResponse.process
-    && readResponse.process.intendedInputs
-    && readResponse.process.intendedInputs[0], iIntentId, 'reciprocal field reference OK on read')
-
-  // ASSERT: test commitment input query edge
-  readResponse = await planning.call('intent_index', 'query_intents', { params: { inputOf: processId } })
-  t.equal(readResponse.edges && readResponse.edges.length, 1, 'field query index present')
-  t.deepEqual(readResponse.edges && readResponse.edges[0] && readResponse.edges[0].node && readResponse.edges[0].node.id, iIntentId, 'query index OK')
-
-  // ASSERT: test process input query edge
-  readResponse = await observation.call('process_index', 'query_processes', { params: { intendedInputs: iIntentId } })
-  t.equal(readResponse.edges && readResponse.edges.length, 1, 'reciprocal query index present')
-  t.deepEqual(readResponse.edges && readResponse.edges[0] && readResponse.edges[0].node && readResponse.edges[0].node.id, processId, 'reciprocal query index OK')
-
-
-
-  // SCENARIO: wipe associated record
-  await planning.call('intent', 'delete_intent', { address: iIntentRevisionId })
-  await s.consistency()
-
-  // ASSERT: test forward link field
-  try {
-    readResponse = await planning.call('intent', 'get_intent', { address: iIntentId })
-  } catch (err) {
-    t.ok(err.data.data.includes('No entry at this address'), 'record deletion OK')
-  }
-
-  // ASSERT: test reciprocal link field
-  readResponse = await observation.call('process', 'get_process', { address: processId })
-  t.equal(readResponse.process
-    && readResponse.process.intendedInputs.length, 0, 'reciprocal field reference removed')
-
-  // ASSERT: test commitment input query edge
-  readResponse = await planning.call('intent_index', 'query_intents', { params: { inputOf: processId } })
-  t.equal(readResponse.edges && readResponse.edges.length, 0, 'field query index removed')
-
-  // ASSERT: test process input query edge
-  readResponse = await observation.call('process_index', 'query_processes', { params: { intendedInputs: iIntentId } })
-  t.equal(readResponse.edges && readResponse.edges.length, 0, 'reciprocal query index removed')
 })
 
 runner.run()
