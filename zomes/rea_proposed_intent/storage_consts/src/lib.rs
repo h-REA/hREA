aefedb072ff3d06c6bcc465a207085f8bf9bfe4c
pospi <pospi@spadgos.com>
2022-05-16 20:28:52 +1000

    remove unneeded storage consts as per #263

--- zomes/rea_proposed_intent/storage_consts/src/lib.rs
+++ zomes/rea_proposed_intent/storage_consts/src/lib.rs
@@ -6,16 +6,3 @@
  * @package Holo-REA
  */
 pub const PROPOSED_INTENT_ENTRY_TYPE: &str = "vf_proposed_intent";
-
-pub const PROPOSED_INTENT_PUBLISHED_IN_LINK_TYPE: &str = "vf_proposed_intent_published_in";
-pub const PROPOSED_INTENT_PUBLISHED_IN_LINK_TAG: &str = "published_in";
-
-pub const PROPOSED_INTENT_PUBLISHES_LINK_TYPE: &str = "vf_proposed_intent_publishes";
-pub const PROPOSED_INTENT_PUBLISHES_LINK_TAG: &str = "publishes";
-
-pub const PROPOSED_INTENT_PROPOSAL_INDEXING_API_METHOD: &str = "_internal_reindex_proposals";
-pub const PROPOSAL_PROPOSED_INTENT_INDEXING_API_METHOD: &str = "_internal_reindex_proposed_intents";
-
-pub const INTENT_PUBLISHEDIN_INDEXING_API_METHOD: &str = "index_intent_proposals";
-
-pub const PROPOSED_INTENT_PROPOSES_INDEXING_API_METHOD: &str = "_internal_index_";
--- zomes/rea_resource_specification/storage_consts/src/lib.rs
+++ zomes/rea_resource_specification/storage_consts/src/lib.rs
@@ -6,7 +6,3 @@
  * @package Holo-REA
  */
 pub const ECONOMIC_RESOURCE_SPECIFICATION_ENTRY_TYPE: &str = "vf_resource_specification";
-pub const ECONOMIC_RESOURCE_SPECIFICATION_BASE_ENTRY_TYPE: &str = "vf_resource_specification_baseurl";
-pub const ECONOMIC_RESOURCE_SPECIFICATION_INITIAL_ENTRY_LINK_TYPE: &str = "vf_resource_specification_entry";
-pub const RESOURCE_SPECIFICATION_CONFORMING_RESOURCE_LINK_TYPE: &str = "vf_resource_specification_conforming_resource";
-pub const RESOURCE_SPECIFICATION_CONFORMING_RESOURCE_LINK_TAG: &str = "conforming_resource";
