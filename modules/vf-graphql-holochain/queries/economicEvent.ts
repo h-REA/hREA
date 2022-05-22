d2a76900e7c8639d4f337a561f6c48e194c93aad
pospi <pospi@spadgos.com>
2022-05-22 21:00:50 +1000

    rebind EconomicEvent & EconomicResource list API endpoints

--- modules/vf-graphql-holochain/queries/economicEvent.ts
+++ modules/vf-graphql-holochain/queries/economicEvent.ts
@@ -15,7 +15,7 @@ import {
 
 export default (dnaConfig: DNAIdMappings, conductorUri: string) => {
   const readOne = mapZomeFn(dnaConfig, conductorUri, 'observation', 'economic_event', 'get_economic_event')
-  const readAll = mapZomeFn(dnaConfig, conductorUri, 'observation', 'economic_event', 'get_all_economic_events')
+  const readAll = mapZomeFn(dnaConfig, conductorUri, 'observation', 'economic_event_index', 'get_all_economic_events')
 
   return {
     economicEvent: injectTypename('EconomicEvent', async (root, args): Promise<EconomicEvent> => {
