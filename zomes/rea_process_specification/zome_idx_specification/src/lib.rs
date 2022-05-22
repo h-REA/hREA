19dc7a09fd02db027a9a6521298449ff55c65078
pospi <pospi@spadgos.com>
2022-05-22 19:18:04 +1000

    fix missing entryDefs for ProcessSpecification & Unit index zomes

--- zomes/rea_process_specification/zome_idx_specification/src/lib.rs
+++ zomes/rea_process_specification/zome_idx_specification/src/lib.rs
@@ -8,6 +8,16 @@ use hdk_semantic_indexes_zome_derive::index_zome;
 use hc_zome_rea_process_specification_rpc::*;
 use hdk_semantic_indexes_zome_lib::ByAddress; // disambiguate from record query struct
 
+// :TODO: remove this; should not be necessary since all these types are imported
+// along with their entry_def! in dependent crates
+#[hdk_extern]
+fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
+    Ok(EntryDefsCallbackResult::from(vec![
+        PathEntry::entry_def(),
+        ProcessSpecificationAddress::entry_def(),
+    ]))
+}
+
 #[index_zome]
 struct ProcessSpecification {
     // :NOTE: blank means only the `read_all_` and `register_new_` APIs will be generated
--- zomes/rea_unit/zome_idx_specification/src/lib.rs
+++ zomes/rea_unit/zome_idx_specification/src/lib.rs
@@ -8,6 +8,16 @@ use hdk_semantic_indexes_zome_derive::index_zome;
 use hc_zome_rea_unit_rpc::*;
 use hdk_semantic_indexes_zome_lib::ByAddress; // disambiguate from record query struct
 
+// :TODO: remove this; should not be necessary since all these types are imported
+// along with their entry_def! in dependent crates
+#[hdk_extern]
+fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
+    Ok(EntryDefsCallbackResult::from(vec![
+        PathEntry::entry_def(),
+        UnitAddress::entry_def(),
+    ]))
+}
+
 #[index_zome]
 struct Unit {
     // :NOTE: blank means only the `read_all_` and `register_new_` APIs will be generated
