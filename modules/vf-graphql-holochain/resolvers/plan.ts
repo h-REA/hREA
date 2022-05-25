b9e2fb0d22b160588842e97127868a7f6cf88fe4
Wesley Finck <wesleyfinck@gmail.com>
2022-05-24 19:37:31 -0700

    added commitment resolver for plan with planning

--- modules/vf-graphql-holochain/resolvers/plan.ts
+++ modules/vf-graphql-holochain/resolvers/plan.ts
@@ -10,21 +10,34 @@ import { extractEdges, mapZomeFn } from '../connection'
 
 import {
   Process,
-  PlanProcessFilterParams
+  PlanProcessFilterParams,
+  Plan,
+  Commitment
 } from '@valueflows/vf-graphql'
 
 
 export default (enabledVFModules: VfModule[] = DEFAULT_VF_MODULES, dnaConfig: DNAIdMappings, conductorUri: string) => {
 
   const hasObservation = -1 !== enabledVFModules.indexOf(VfModule.Observation)
+  const hasPlanning = -1 !== enabledVFModules.indexOf(VfModule.Planning)
 
   const readProcesses = mapZomeFn(dnaConfig, conductorUri, 'observation', 'process_index', 'query_processes')
+  const queryCommitments = mapZomeFn(dnaConfig, conductorUri, 'planning', 'commitment_index', 'query_commitments')
 
   return Object.assign(
     (hasObservation ? {
       processes: async (filter: PlanProcessFilterParams): Promise<Process[]> => {
         const results = await readProcesses(filter)
         return extractEdges(results)
+      },
+    } : {}),
+    (hasPlanning ? {
+      independentDemands: async (record: Plan): Promise<Commitment[]> => {
+        const commitments = await queryCommitments({ params: { clauseOf: record.id } })
+        if (!commitments.edges || !commitments.edges.length) {
+          return []
+        }
+        return commitments.edges.map(({ node }) => node)
       },
     } : {}),
   )
