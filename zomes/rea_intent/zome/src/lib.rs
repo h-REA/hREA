ea134a2fec0ac903339492cfd77475d73c82ca83
pospi <pospi@spadgos.com>
2022-06-08 22:37:36 +1000

    HDK changes for EntryDef

--- zomes/rea_intent/zome/src/lib.rs
+++ zomes/rea_intent/zome/src/lib.rs
@@ -48,14 +48,12 @@ fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
         EntryDef {
             id: CAP_STORAGE_ENTRY_DEF_ID.into(),
             visibility: EntryVisibility::Private,
-            crdt_type: CrdtType,
             required_validations: 1.into(),
             required_validation_type: RequiredValidationType::default(),
         },
         EntryDef {
             id: INTENT_ENTRY_TYPE.into(),
             visibility: EntryVisibility::Public,
-            crdt_type: CrdtType,
             required_validations: 2.into(),
             required_validation_type: RequiredValidationType::default(),
         }
