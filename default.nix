a731bdd7d4f43cc8f0061a2b0387c5f046cd18d2
pospi <pospi@spadgos.com>
2022-06-08 22:35:40 +1000

    package version updates for latest HDK

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
-  holonixRev = "52158409f9b76b442e592e8f06632b0e57a6c365";
+  holonixRev = "c7a0b2c75480d429d570c94909cec3210280ad4c";
 
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_127";
+    holochainVersionId = "v0_0_143";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
