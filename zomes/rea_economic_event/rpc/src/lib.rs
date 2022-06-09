7594488c20631151904a0fc03619c43fd2224e11
Connor Turland <connor@sprillow.com>
2022-06-09 09:50:44 -0700

    fixes #288

--- zomes/rea_economic_event/rpc/src/lib.rs
+++ zomes/rea_economic_event/rpc/src/lib.rs
@@ -96,6 +96,8 @@ pub struct ResourceResponse {
     pub id: EconomicResourceAddress,
     pub revision_id: HeaderHash,
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub conforms_to: Option<ResourceSpecificationAddress>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub classified_as: Option<Vec<ExternalURL>>,
