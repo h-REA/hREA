91c2f250b0ec66c4aedf5538514bdfc7d583398f
Wesley Finck <wesleyfinck@gmail.com>
2022-06-08 19:12:15 -0700

    updated agent queries

--- modules/vf-graphql-holochain/queries/agent.ts
+++ modules/vf-graphql-holochain/queries/agent.ts
@@ -7,13 +7,14 @@
  * @since:   2020-02-19
  */
 
-import { DNAIdMappings, injectTypename } from '../types'
+import { DNAIdMappings, injectTypename, ReadParams } from '../types'
 import { mapZomeFn, serializeHash, deserializeHash } from '../connection'
 
 import {
   Agent
 } from '@valueflows/vf-graphql'
 import { AgentPubKey } from '@holochain/client'
+import { AgentResponse } from '../mutations/agent'
 
 export interface RegistrationQueryParams {
   pubKey: AgentPubKey,
@@ -21,66 +22,21 @@ export interface RegistrationQueryParams {
 
 export default (dnaConfig: DNAIdMappings, conductorUri: string) => {
 
-  const readMyAgent = mapZomeFn<null, AgentPubKey>(dnaConfig, conductorUri, 'agent', 'agent_registration', 'get_my_agent_pubkey')
-  const readAllAgents = mapZomeFn<null, AgentPubKey[]>(dnaConfig, conductorUri, 'agent', 'agent_registration', 'get_registered')
-  // special 'true' at the end is for skipEncodeDecode, because of the way this zome handles serialization and inputs
-  // which is different from others
-  const agentExists = mapZomeFn<RegistrationQueryParams, boolean>(dnaConfig, conductorUri, 'agent', 'agent_registration', 'is_registered', true)
+  //assumes there is a link from agentPubKey to a Person entry, but what if link cannot be resolved?
+  const readMyAgent = mapZomeFn<null, Agent>(dnaConfig, conductorUri, 'agent', 'agent', 'get_my_agent')
+  const readAgent = mapZomeFn<ReadParams, AgentResponse>(dnaConfig, conductorUri, 'agent', 'agent', 'get_agent')
 
   // read mapped DNA hash in order to construct VF-native IDs from DNA-local HC IDs
   const mappedDNA = dnaConfig['agent'] ? serializeHash(dnaConfig['agent'][0]) : null
 
   return {
     // :TODO: is myAgent always a 'Person' in Holochain, or will we allow users to act in an Organization context directly?
-    myAgent: injectTypename('Person', async (root, args): Promise<Agent> => {
-      const agentPubKey = serializeHash(await readMyAgent(null))
-      // :TODO: wire to Personas hApp
-      return {
-        id: `${agentPubKey}:${mappedDNA}`,
-        revisionId: '',
-        name: `Agent ${agentPubKey.substr(2, 4)}`,
-        meta: {
-          retrievedRevision: {
-            id: '',
-          }
-        }
-      }
-    }),
-
-    // :TODO: this and the associated functionality in 'get_registered' needs to be revisited
-    // or potentially integrated from other projects affording similar functionality.
-    agents: async (root, args): Promise<Agent[]> => {
-      return (await readAllAgents(null)).map(agentAddress => ({
-        // :TODO: wire to Personas hApp
-        id: `${serializeHash(agentAddress)}:${mappedDNA}`,
-        revisionId: '',
-        name: `Agent ${serializeHash(agentAddress).substr(2, 4)}`,
-        __typename: 'Person',  // :SHONK:
-        meta: {
-          retrievedRevision: {
-            id: ''
-          }
-        }
-      }))
+    myAgent: async (root, args): Promise<Agent> => {
+      return readMyAgent(null) 
     },
 
-    agent: injectTypename('Person', async (root, { id }): Promise<Agent> => {
-      const rawAgentPubKey = deserializeHash(id.split(':')[0])
-      const isAgent = await agentExists({ pubKey: rawAgentPubKey })
-
-      if (!isAgent) {
-        throw new Error('No agent exists with that ID')
-      }
-      return {
-        id,
-        revisionId: '',
-        name: `Agent ${id.substr(2, 4)}`,
-        meta: {
-          retrievedRevision: {
-            id: '',
-          }
-        }
-      }
+    agent: injectTypename('Agent', async (root, args): Promise<Agent> => {
+      return (await readAgent({ address: args.id })).agent
     }),
   }
 }
