ff5fc3899cb6f65085378abf2a8ae8fb9cb81775
Wesley Finck <wesleyfinck@gmail.com>
2022-06-15 15:35:34 -0700

    WIP: agent test crud file

---
+++ test/agent/test_agent_crud.js
@@ -0,0 +1,148 @@
+import test from 'tape'
+import { pause } from '@connoropolous/tryorama'
+import { buildPlayer } from '../init.js'
+
+const examplePerson = {
+  name: 'test person',
+  image: 'https://image.png',
+  note: 'test person note',
+}
+const updatedPerson = {
+  name: 'updated person',
+  image: 'https://image2.png',
+  note: 'updated the person to something else',
+}
+
+test('Agent record API', async (t) => {
+  const alice = await buildPlayer(['agent'])
+
+  let createResp = await alice.graphQL(
+    `
+    mutation($rs: AgentCreateParams!) {
+      res: createPerson(person: $rs) {
+        agent {
+          id
+          revisionId
+          name
+          image
+          note
+        }
+      }
+    }
+  `,
+    {
+      rs: examplePerson,
+    },
+  )
+  await pause(100)
+  console.log('created person response: ', createResp)
+  t.ok(createResp.data.res.agent.id, 'record created')
+  const pId = createResp.data.res.agent.id
+  const r1Id = createResp.data.res.agent.revisionId
+
+  let getResp = await alice.graphQL(
+    `
+    query($id: ID!) {
+      res: agent(id: $id) {
+        id
+        revisionId
+        name
+        image
+        note
+      }
+    }
+  `,
+    {
+      id: pId,
+    },
+  )
+  t.deepLooseEqual(
+    getResp.data.res, { id: pId, revisionId: r1Id, ...examplePerson },
+    'record read OK',
+  )
+
+  const updateResp = await alice.graphQL(
+    `
+    mutation($rs: AgentUpdateParams!) {
+      res: updatePerson(person: $rs) {
+        agent {
+          id
+          revisionId
+        }
+      }
+    }
+  `,
+    {
+      rs: { revisionId: r1Id, ...updatedPerson },
+    },
+  )
+  await pause(100)
+  t.equal(updateResp.data.res.agent.id, pId, 'record updated')
+  const r2Id = updateResp.data.res.agent.revisionId
+
+  // now we fetch the Entry again to check that the update was successful
+  const updatedGetResp = await alice.graphQL(
+    `
+    query($id: ID!) {
+      res: agent(id: $id) {
+        id
+        revisionId
+        name
+        image
+        note
+      }
+    }
+  `,
+    {
+      id: pId,
+    },
+  )
+  t.deepLooseEqual(
+    updatedGetResp.data.res,
+    {
+      id: pId,
+      revisionId: r2Id,
+      created: examplePerson.created,
+      ...updatedPerson,
+    },
+    'record updated OK',
+  )
+
+  const deleteResult = await alice.graphQL(
+    `
+    mutation($id: ID!) {
+      res: deletePerson(revisionId: $id)
+    }
+  `,
+    {
+      id: r2Id,
+    },
+  )
+  await pause(100)
+  t.equal(deleteResult.data.res, true)
+
+  const queryForDeleted = await alice.graphQL(
+    `
+    query($id: ID!) {
+      res: agent(id: $id) {
+        id
+      }
+    }
+  `,
+    {
+      id: pId,
+    },
+  )
+  t.equal(
+    queryForDeleted.errors.length,
+    1,
+    'querying deleted record is an error',
+  )
+  t.notEqual(
+    -1,
+    queryForDeleted.errors[0].message.indexOf('No entry at this address'),
+    'correct error reported',
+  )
+
+  await alice.scenario.cleanUp()
+})
