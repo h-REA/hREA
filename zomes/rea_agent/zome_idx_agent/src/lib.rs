ff5e5685d9897e13b9aa1f03daead7370dafe35a
Wesley Finck <wesleyfinck@gmail.com>
2022-06-23 15:09:02 -0700

    added entry defs to zom_idx_agent to match other crates

--- zomes/rea_agent/zome_idx_agent/src/lib.rs
+++ zomes/rea_agent/zome_idx_agent/src/lib.rs
@@ -7,6 +7,16 @@
 use hdk_semantic_indexes_zome_derive::index_zome;
 use hc_zome_rea_agent_rpc::*;
 
+// :TODO: remove this; should not be necessary since all these types are imported
+// along with their entry_def! in dependent crates
+#[hdk_extern]
+fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
+    Ok(EntryDefsCallbackResult::from(vec![
+        PathEntry::entry_def(),
+        CommitmentAddress::entry_def(),
+        CommitmentAddress::entry_def(),
+    ]))
+}
 #[index_zome]
 struct Agent {
     // internal indexes (not part of VF spec)
