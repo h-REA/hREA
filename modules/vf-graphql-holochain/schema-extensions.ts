5f352756779064a8e67b524ff40348c731a89db7
Connor Turland <connor@sprillow.com>
2022-06-28 18:02:42 -0700

    add comment to schema extension

--- modules/vf-graphql-holochain/schema-extensions.ts
+++ modules/vf-graphql-holochain/schema-extensions.ts
@@ -1,5 +1,6 @@
 const associateMyAgentExtension = `
 type Mutation  {
+    "Associates the Agent identified by agentId with the currently authenticated user. Can only be used once."
     associateMyAgent(agentId: ID!): Boolean!
 }
 `;
