7d8fd217421be17cc7e8ec8d1c65bd8218640340
Wesley Finck <wesleyfinck@gmail.com>
2022-07-05 15:01:46 -0700

    resource specification query all endpoint and passing test

--- modules/vf-graphql-holochain/queries/resourceSpecification.ts
+++ modules/vf-graphql-holochain/queries/resourceSpecification.ts
@@ -9,15 +9,20 @@ import { DNAIdMappings, ReadParams } from '../types.js'
 import { mapZomeFn } from '../connection.js'
 
 import {
-  ResourceSpecification, ResourceSpecificationResponse,
+  ResourceSpecification, ResourceSpecificationConnection, ResourceSpecificationResponse,
 } from '@valueflows/vf-graphql'
+import { PagingParams } from '../resolvers/zomeSearchInputTypes.js'
 
 export default (dnaConfig: DNAIdMappings, conductorUri: string) => {
   const readOne = mapZomeFn<ReadParams, ResourceSpecificationResponse>(dnaConfig, conductorUri, 'specification', 'resource_specification', 'get_resource_specification')
+  const readAll = mapZomeFn<PagingParams, ResourceSpecificationConnection>(dnaConfig, conductorUri, 'specification', 'resource_specification_index', 'read_all_resource_specifications')
 
   return {
     resourceSpecification: async (root, args): Promise<ResourceSpecification> => {
       return (await readOne({ address: args.id })).resourceSpecification
+    },
+    resourceSpecifications: async (root, args: PagingParams): Promise<ResourceSpecificationConnection> => {
+      return await readAll(args)
     },
   }
 }
