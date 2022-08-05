a2aaad15ddf8c9612484489f32bd5ef8aaeac2e2
Wesley Finck <wesleyfinck@gmail.com>
2022-08-05 16:14:06 -0700

    removed index segment entry definition from semantic indexes integrity zome

--- lib/hdk_semantic_indexes/integrity_zome/src/lib.rs
+++ lib/hdk_semantic_indexes/integrity_zome/src/lib.rs
@@ -5,14 +5,3 @@ pub enum LinkTypes {
     SemanticIndex,
     TimeIndex,
 }
-
-#[hdk_entry_defs]
-#[unit_enum(UnitEntryType)]
-pub enum EntryTypes {
-    IndexSegment(IndexSegment),
-}
-
-// does this need an entry def id of "time_index"
-#[hdk_entry_helper]
-#[derive(Clone)]
-pub struct IndexSegment(u64);
