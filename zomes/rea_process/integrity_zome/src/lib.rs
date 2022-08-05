6095b96c75303cb7da6f078af74269fb939d9687
Wesley Finck <wesleyfinck@gmail.com>
2022-08-04 18:15:09 -0700

    use integrity zomes for observation dna

---
+++ zomes/rea_process/integrity_zome/src/lib.rs
@@ -0,0 +1,11 @@
+/**
+ * Holo-REA process integrity zome for API definition
+ *
+ * Defines the top-level zome configuration needed by Holochain's build system
+ * to bundle the app. This basically involves wiring up the helper methods from the
+ * related `_lib` module into a packaged zome WASM binary.
+ *
+ * @package Holo-REA
+ */
+
+pub use hc_zome_rea_process_storage::EntryTypes;
