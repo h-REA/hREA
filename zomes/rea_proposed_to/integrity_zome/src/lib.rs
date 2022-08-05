8023439f55381cbb16363f5a595a7d1d1024d1be
Wesley Finck <wesleyfinck@gmail.com>
2022-08-05 15:35:52 -0700

    use integrity zomes for proposal dna

---
+++ zomes/rea_proposed_to/integrity_zome/src/lib.rs
@@ -0,0 +1,23 @@
+/**
+ * Holo-REA proposed to integrity zome for API definition
+ *
+ * Defines the top-level zome configuration needed by Holochain's build system
+ * to bundle the app. This basically involves wiring up the helper methods from the
+ * related `_lib` module into a packaged zome WASM binary.
+ *
+ * @package Holo-REA
+ */
+
+pub use hc_zome_rea_proposed_to_storage::EntryStorage;
+#[hdk_entry_defs]
+#[unit_enum(UnitEntryType)]
+pub enum EntryTypes {
+    ProposedTo(EntryStorage),
+}
+impl From<EntryStorage> for EntryTypes
+{
+    fn from(e: EntryStorage) -> EntryTypes
+    {
+        EntryTypes::ProposedTo(e)
+    }
+}
