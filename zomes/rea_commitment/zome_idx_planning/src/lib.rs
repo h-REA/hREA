4c916fa52f2bc7f083e0b76642d731f5dd4fc032
pospi <pospi@spadgos.com>
2022-08-07 12:43:41 +0200

    remove old EntryDefs from all index zomes, now serviced by dependency in integrity crate

--- zomes/rea_commitment/zome_idx_planning/src/lib.rs
+++ zomes/rea_commitment/zome_idx_planning/src/lib.rs
@@ -7,24 +7,6 @@
 use hdk_semantic_indexes_zome_derive::index_zome;
 use hc_zome_rea_commitment_rpc::*;
 
-// :TODO: remove this; should not be necessary since all these types are imported
-// along with their entry_def! in dependent crates
-#[hdk_extern]
-fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
-    Ok(EntryDefsCallbackResult::from(vec![
-        PathEntry::entry_def(),
-        TimeIndex::entry_def(),
-        ProcessAddress::entry_def(),
-        SatisfactionAddress::entry_def(),
-        CommitmentAddress::entry_def(),
-        ProcessAddress::entry_def(),
-        AgreementAddress::entry_def(),
-        FulfillmentAddress::entry_def(),
-        PlanAddress::entry_def(),
-        AgentAddress::entry_def(),
-    ]))
-}
-
 #[index_zome]
 struct Commitment {
     fulfilled_by: Local<fulfillment, fulfills>,
--- zomes/rea_economic_event/zome_idx_observation/src/lib.rs
+++ zomes/rea_economic_event/zome_idx_observation/src/lib.rs
@@ -7,23 +7,6 @@
 use hdk_semantic_indexes_zome_derive::index_zome;
 use hc_zome_rea_economic_event_rpc::*;
 
-// :TODO: remove this; should not be necessary since all these types are imported
-// along with their entry_def! in dependent crates
-#[hdk_extern]
-fn entry_defs(_: ()) -> ExternResult<EntryDefsCallbackResult> {
-    Ok(EntryDefsCallbackResult::from(vec![
-        PathEntry::entry_def(),
-        TimeIndex::entry_def(),
-        ProcessAddress::entry_def(),
-        AgreementAddress::entry_def(),
-        SatisfactionAddress::entry_def(),
-        FulfillmentAddress::entry_def(),
-        EconomicResourceAddress::entry_def(),
-        EconomicEventAddress::entry_def(),
-        AgentAddress::entry_def(),
-    ]))
-}
-
 #[index_zome]
 struct EconomicEvent {
     input_of: Local<process, inputs>,
