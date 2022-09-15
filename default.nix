713480e42e4d6d0e230e0d7867fb9a6d99e48a41
Connor Turland <connor@sprillow.com>
2022-09-15 05:57:32 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
-  holonixRev = "0b1bf0eb19626f354c8efce70a21dbdbbaffbc71";
+  holonixRev = "38f74c722048f787d9faadb479978e73bb5091a1";
 
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_161";
+    holochainVersionId = "v0_0_162";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
