920b5a3d2d42b26b42f69aa820b0bf6656ca866a
Connor Turland <connor@sprillow.com>
2023-01-19 00:02:57 -0800

    Update run-dev-conductor.sh

--- scripts/run-dev-conductor.sh
+++ scripts/run-dev-conductor.sh
@@ -12,9 +12,9 @@ set -e
 
 UTIL="${HOLOCHAIN_DNA_UTIL_PATH:-hc}"
 APP="${HOLOCHAIN_APP_PORT:-4000}"
-# ADM="${HOLOCHAIN_ADMIN_PORT:-4001}"
+ADM="${HOLOCHAIN_ADMIN_PORT:-4001}"
 
 "$UTIL" s clean
 "$UTIL" s create -n 1 -d hrea_tester network quic
 "$UTIL" s call install-app ./bundles/app/full_suite/hrea_suite.happ
-"$UTIL" s run --all -p $APP
+"$UTIL" s -f=$ADM run --all -p $APP
