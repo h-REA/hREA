036c79e77d4e4698fc53a31851f8ca51cfd132b0
pospi <pospi@spadgos.com>
2023-02-23 15:55:13 +1000

    switch to Nix flakes for development, remove old Nix config

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Flake for Holochain app development";
+
+  inputs = {
+    nixpkgs.follows = "holochain-dev/nixpkgs";
+
+    holochain-dev = {
+      url = "github:holochain/holochain";
+      inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
+    };
+  };
+
+  outputs = inputs @ { ... }:
+    inputs.holochain-dev.inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        systems = builtins.attrNames inputs.holochain-dev.devShells;
+        perSystem =
+          { config
+          , pkgs
+          , system
+          , ...
+          }: {
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ inputs.holochain-dev.devShells.${system}.holonix ];
+              packages = with pkgs; [
+                nodejs-18_x
+                nodePackages.npm
+              ];
+            };
+          };
+      };
+}
