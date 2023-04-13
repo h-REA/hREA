134fd96143e437b065de51f325280a74a3c67022
pospi <pospi@spadgos.com>
2023-04-13 11:52:57 +1000

    pin Holochain correctly, update to v0.1.3

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     holochain-dev = {
       url = "github:holochain/holochain";
       inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
+      inputs.holochain.url = "github:holochain/holochain/holochain-0.1.3";
     };
   };
 
