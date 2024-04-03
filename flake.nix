beb230004612d5654e2d54404dfd7283cdc5c9ea
Leo <leobensman@gmail.com>
2024-04-03 15:10:45 +0200

    update crates and packages

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Template for Holochain app development";
 
   inputs = {
-    versions.url  = "github:holochain/holochain?dir=versions/0_2";
-
-    holochain-flake.url = "github:holochain/holochain";
-    holochain-flake.inputs.versions.follows = "versions";
-    holochain-flake.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.1";
+    holochain-nix-versions.url  = "github:holochain/holochain/?dir=versions/weekly";
+    holochain-flake = {
+      url = "github:holochain/holochain";
+      inputs.versions.follows = "holochain-nix-versions";
+    };
 
     nixpkgs.follows = "holochain-flake/nixpkgs";
     flake-parts.follows = "holochain-flake/flake-parts";
