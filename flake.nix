a92c4818ee11869e35db61575f21f2c22900c2e6
Leo <leobensman@gmail.com>
2024-05-15 17:46:26 +0200

    update nix flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Template for Holochain app development";
 
   inputs = {
-    holochain-nix-versions.url  = "github:holochain/holochain/?dir=versions/weekly";
-    holochain-flake = {
-      url = "github:holochain/holochain";
-      inputs.versions.follows = "holochain-nix-versions";
-    };
+    versions.url  = "github:holochain/holochain?dir=versions/0_3_rc";
+
+    holochain-flake.url = "github:holochain/holochain/";
+    holochain-flake.inputs.versions.follows = "versions";
+    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.46";
 
     nixpkgs.follows = "holochain-flake/nixpkgs";
     flake-parts.follows = "holochain-flake/flake-parts";
