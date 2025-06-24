57d30c59b6750380a5aa1a55534d8e3c274bbd28
Leo <leobensman@gmail.com>
2025-06-24 22:18:41 +0200

    upgrade to holochain 0.5

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Holochain app development";
 
   inputs = {
-    holonix.url = "github:holochain/holonix?ref=main-0.4";
+    holonix.url = "github:holochain/holonix?ref=main-0.5";
 
     nixpkgs.follows = "holonix/nixpkgs";
     flake-parts.follows = "holonix/flake-parts";
@@ -18,6 +18,8 @@
 
         packages = (with inputs'.holonix.packages; [
           holochain
+          hc
+          bootstrap-srv
           lair-keystore
           hc-launch
           hc-scaffold
